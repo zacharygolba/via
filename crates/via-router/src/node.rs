b255726774aef81a2cf269d0bc0f5f2cddd7de0f
Zachary Golba <pino@hey.com>
2024-06-03 14:04:27 -0400

    Support matching multiple routes
    Support matching multiple routes

--- crates/via-router/src/node.rs
+++ crates/via-router/src/node.rs
@@ -1,11 +1,19 @@
-use smallvec::SmallVec;
-use std::cmp::{Ordering, PartialOrd};
+use std::{
+    cmp::{Ordering, PartialOrd},
+    slice,
+};
 
 #[derive(Clone, Debug)]
 pub struct Node<T> {
-    pub(crate) entries: SmallVec<[Box<Self>; 4]>,
+    pub(crate) entries: Vec<Box<Self>>,
     pub(crate) pattern: Pattern,
     pub(crate) route: T,
+}
+
+#[derive(Debug)]
+pub struct Visitor<'a, 'b, T> {
+    entries: slice::Iter<'a, Box<Node<T>>>,
+    predicate: &'b str,
 }
 
 #[non_exhaustive]
@@ -18,19 +26,61 @@ pub enum Pattern {
 }
 
 impl<T: Default> Node<T> {
-    pub fn find(&self, path: &str) -> Option<&Self> {
-        self.entries.iter().find_map(|node| {
-            if node.pattern == *path {
-                Some(&**node)
-            } else {
-                None
-            }
-        })
+    fn new(pattern: Pattern) -> Node<T> {
+        Node {
+            pattern,
+            entries: Vec::new(),
+            route: Default::default(),
+        }
+    }
+
+    pub fn find<F>(&self, offset: usize, mut predicate: F) -> Option<(usize, &Node<T>)>
+    where
+        F: FnMut(&Node<T>) -> bool,
+    {
+        println!("    find from offset: {}", offset);
+
+        self.entries
+            .iter()
+            .skip(offset)
+            .enumerate()
+            .find_map(|(index, node)| {
+                if predicate(node) {
+                    println!("        index: {}, node: {:?}", index, node.pattern);
+                    Some((offset + index, &**node))
+                } else {
+                    None
+                }
+            })
     }
 
     pub fn index(&self, pattern: Pattern) -> Option<usize> {
         self.entries.iter().position(|node| pattern == node.pattern)
     }
+
+    // pub fn insert<I>(&mut self, segments: &mut I) -> &mut Self
+    // where
+    //     I: Iterator<Item = Pattern>,
+    // {
+    //     if let Pattern::CatchAll(_) = self.pattern {
+    //         return self;
+    //     }
+
+    //     let pattern = match segments.next() {
+    //         Some(value) => value,
+    //         None => return self,
+    //     };
+
+    //     if let Some(index) = self.index(pattern) {
+    //         self.entries[index].insert(segments)
+    //     } else {
+    //         let index = self.entries.len();
+    //         let entry = Node::new(pattern);
+
+    //         self.entries.push(Box::new(entry));
+    //         &mut self.entries[index]
+    //     }
+    // }
 
     pub fn insert<I>(&mut self, segments: &mut I) -> &mut Self
     where
@@ -40,14 +90,14 @@ impl<T: Default> Node<T> {
             return self;
         }
 
-        let label = match segments.next() {
+        let pattern = match segments.next() {
             Some(value) => value,
             None => return self,
         };
 
-        let index = match self.index(label) {
+        let index = match self.index(pattern) {
             Some(value) => value,
-            None => insert1(self, label),
+            None => insert1(self, pattern),
         };
 
         self.entries[index].insert(segments)
@@ -57,9 +107,18 @@ impl<T: Default> Node<T> {
 impl<T: Default> Default for Node<T> {
     fn default() -> Node<T> {
         Node {
-            entries: SmallVec::new(),
+            entries: Vec::new(),
             pattern: Pattern::Root,
             route: Default::default(),
+        }
+    }
+}
+
+impl Pattern {
+    pub fn name(&self) -> Option<&'static str> {
+        match self {
+            Pattern::CatchAll(name) | Pattern::Dynamic(name) => Some(name),
+            _ => None,
         }
     }
 }
@@ -108,27 +167,49 @@ impl PartialOrd for Pattern {
     }
 }
 
-fn insert1<T: Default>(node: &mut Node<T>, pattern: Pattern) -> usize {
-    let mut offset = 0;
+impl<'a, 'b, T> Iterator for Visitor<'a, 'b, T> {
+    type Item = &'a Node<T>;
 
-    for (index, entry) in node.entries.iter().enumerate() {
-        offset = match entry.pattern {
-            Pattern::Static(_) => index + 1,
-            _ => index,
-        };
+    fn next(&mut self) -> Option<Self::Item> {
+        let next = self.entries.next()?;
 
-        if pattern < entry.pattern {
-            break;
+        if next.pattern == *self.predicate {
+            Some(next)
+        } else {
+            None
         }
     }
+}
 
-    node.entries.insert(
-        offset,
-        Box::new(Node {
-            pattern,
-            ..Default::default()
-        }),
-    );
+fn insert1<T: Default>(node: &mut Node<T>, pattern: Pattern) -> usize {
+    // let mut offset = 0;
+
+    // for (index, entry) in node.entries.iter().enumerate() {
+    //     offset = match entry.pattern {
+    //         Pattern::Static(_) => index + 1,
+    //         _ => index,
+    //     };
+
+    //     if pattern < entry.pattern {
+    //         break;
+    //     }
+    // }
+
+    let offset = node.entries.len();
+    node.entries.push(Box::new(Node {
+        pattern,
+        ..Default::default()
+    }));
+
+    assert!(node.entries[offset].pattern == pattern);
+
+    // node.entries.insert(
+    //     offset,
+    //     Box::new(Node {
+    //         pattern,
+    //         ..Default::default()
+    //     }),
+    // );
 
     offset
 }
