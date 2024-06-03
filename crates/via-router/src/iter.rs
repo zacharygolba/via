536c8a37ea48b8a309491fefa78b57b982eb1b6f
Zachary Golba <pino@hey.com>
2024-06-03 14:04:27 -0400

    Support matching multiple routes
    Support matching multiple routes

--- crates/via-router/src/iter.rs
+++ crates/via-router/src/iter.rs
@@ -1,4 +1,4 @@
-use std::{iter::Peekable, ops::Deref, str::CharIndices};
+use std::{iter::Peekable, ops::Deref, rc::Rc, str::CharIndices};
 
 use crate::node::{Node, Pattern};
 
@@ -13,26 +13,17 @@ pub struct Component<'a, 'b, T> {
 #[derive(Debug)]
 pub struct Visit<'a, 'b, T> {
     node: &'a Node<T>,
-    path: Path<'b>,
-    root: bool,
+    depth: usize,
+    index: usize,
+    path_value: &'b str,
+    path_segments: Rc<Vec<(usize, &'b str)>>,
+    visitor_delegate: Option<Box<Self>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub(crate) struct Path<'a> {
-    iter: Peekable<CharIndices<'a>>,
-    next: Option<(usize, &'a str)>,
+    chars: Peekable<CharIndices<'a>>,
     value: &'a str,
-}
-
-impl<'a, 'b, T> Component<'a, 'b, T> {
-    pub(crate) fn root(route: &'a T, is_exact_match: bool) -> Component<'a, 'b, T> {
-        Component {
-            pattern: Pattern::Root,
-            param: None,
-            is_exact_match,
-            route,
-        }
-    }
 }
 
 impl<'a, 'b, T> Deref for Component<'a, 'b, T> {
@@ -44,53 +35,17 @@ impl<'a, 'b, T> Deref for Component<'a, 'b, T> {
 }
 
 impl<'a> Path<'a> {
-    pub fn parse(value: &'a str) -> Path<'a> {
+    pub fn new(value: &'a str) -> Path<'a> {
         Path {
-            iter: value.char_indices().peekable(),
-            next: None,
+            chars: value.char_indices().peekable(),
             value,
         }
-    }
-
-    pub fn peek(&mut self) -> Option<(usize, &'a str)> {
-        match self.next {
-            next @ Some(_) => next,
-            None => {
-                self.next = self.advance();
-                self.next
-            }
-        }
-    }
-
-    pub fn slice(&self, from: usize) -> &'a str {
-        &self.value[from..]
-    }
-
-    fn advance(&mut self) -> Option<(usize, &'a str)> {
-        let mut start = None;
-        let mut end = self.value.len();
-
-        while let (index, '/') = *self.iter.peek()? {
-            start = Some(index);
-            self.iter.next();
-        }
-
-        while let Some((index, value)) = self.iter.peek() {
-            if *value == '/' {
-                end = *index;
-                break;
-            }
-
-            self.iter.next();
-        }
-
-        Some((start?, &self.value[(start? + 1)..end]))
     }
 }
 
 impl Path<'static> {
     pub fn segments(source: &'static str) -> impl Iterator<Item = Pattern> {
-        Path::parse(source).map(|(_, segment)| segment.into())
+        Path::new(source).map(|(_, segment)| segment.into())
     }
 }
 
@@ -98,7 +53,24 @@ impl<'a> Iterator for Path<'a> {
     type Item = (usize, &'a str);
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.next.take().or_else(|| self.advance())
+        let mut start = None;
+        let mut end = self.value.len();
+
+        while let (index, '/') = *self.chars.peek()? {
+            start = Some(index);
+            self.chars.next();
+        }
+
+        while let Some((index, value)) = self.chars.peek() {
+            if *value == '/' {
+                end = *index;
+                break;
+            }
+
+            self.chars.next();
+        }
+
+        Some((start?, &self.value[(start? + 1)..end]))
     }
 }
 
@@ -109,12 +81,52 @@ impl<'a> PartialEq<&'_ str> for Path<'a> {
 }
 
 impl<'a, 'b, T: Default> Visit<'a, 'b, T> {
-    pub fn root(node: &'a Node<T>, path: &'b str) -> Self {
+    pub(crate) fn root(node: &'a Node<T>, path: &'b str) -> Self {
+        let path_segments = Path::new(path).collect();
+
         Visit {
             node,
-            path: Path::parse(path),
-            root: true,
+            depth: 0,
+            index: 0,
+            path_value: path,
+            path_segments: Rc::new(path_segments),
+            visitor_delegate: None,
         }
+    }
+
+    fn fork(&self, node: &'a Node<T>) -> Box<Self> {
+        Box::new(Visit {
+            node,
+            index: 0,
+            depth: self.depth + 1,
+            path_value: self.path_value,
+            path_segments: Rc::clone(&self.path_segments),
+            visitor_delegate: None,
+        })
+    }
+
+    fn is_last(&self) -> bool {
+        self.depth == self.path_segments.len() - 1
+    }
+
+    fn delegate_next(&mut self) -> Option<Component<'a, 'b, T>> {
+        self.visitor_delegate
+            .as_mut()
+            .and_then(|delegate| delegate.next())
+            .or_else(|| {
+                self.visitor_delegate = None;
+                None
+            })
+    }
+
+    fn get_path_segment_value(&self) -> Option<&'b str> {
+        self.path_segments.get(self.depth).map(|(_, value)| *value)
+    }
+
+    fn get_remaining_path(&self) -> Option<&'b str> {
+        self.path_segments
+            .get(self.depth)
+            .map(|(start, _)| &self.path_value[*start..])
     }
 }
 
@@ -122,26 +134,50 @@ impl<'a, 'b, T: Default> Iterator for Visit<'a, 'b, T> {
     type Item = Component<'a, 'b, T>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.root {
-            self.root = false;
-            return Some(Component::root(&self.node.route, self.path == "/"));
+        println!(
+            "Visit::next pattern = {:?}, depth = {}, index = {}",
+            self.node.pattern, self.depth, self.index
+        );
+
+        // First, delegate to the next visitor to see if there are any matches
+        // from decedent nodes.
+        if let Some(component) = self.delegate_next() {
+            return Some(component);
         }
 
-        let Visit { node, path, .. } = self;
-        // Rather than returning early if there are no more path segments, we
-        // must provide a default value to support resolving immediate children
-        // of the root node.
-        let (start, value) = path.next().unwrap_or((0, ""));
-        let next = node.find(value)?;
+        if self.get_path_segment_value().is_none() {
+            let (index, next) = self.node.find(self.index, |entry| {
+                matches!(entry.pattern, Pattern::Root | Pattern::CatchAll(_))
+            })?;
 
-        *node = next;
+            self.index = index + 1;
+
+            return Some(Component {
+                is_exact_match: true,
+                pattern: next.pattern,
+                param: Some((next.pattern.name().unwrap(), "")),
+                route: &next.route,
+            });
+        }
+
+        let path_segment = self.get_path_segment_value()?;
+        let (index, next) = self
+            .node
+            .find(self.index, |entry| entry.pattern == *path_segment)?;
+        let is_catch_all = matches!(next.pattern, Pattern::CatchAll(_));
+
+        self.index = index + 1;
+
+        if !is_catch_all {
+            self.visitor_delegate = Some(self.fork(next));
+        }
 
         Some(Component {
-            is_exact_match: path.peek().is_none(),
+            is_exact_match: is_catch_all || self.is_last(),
             pattern: next.pattern,
             param: match next.pattern {
-                Pattern::CatchAll(name) => Some((name, path.slice(start))),
-                Pattern::Dynamic(name) => Some((name, value)),
+                Pattern::CatchAll(param_name) => Some((param_name, self.get_remaining_path()?)),
+                Pattern::Dynamic(param_name) => Some((param_name, path_segment)),
                 _ => None,
             },
             route: &next.route,
