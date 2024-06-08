dc22ee563e2bf3281557b7441c2d73f37c03256c
Zachary Golba <pino@hey.com>
2024-06-08 13:50:50 -0400

    Update the blog example to use the new api

---
+++ docs/examples/advanced-blog/src/api/posts.rs
@@ -0,0 +1,45 @@
+use via::prelude::*;
+
+use super::Document;
+use crate::database::{models::post::*, Pool};
+
+pub async fn authenticate(context: Context, next: Next) -> Result<impl IntoResponse> {
+    println!("authenticate");
+    next.call(context).await
+}
+
+pub async fn index(context: Context, _: Next) -> Result<impl IntoResponse> {
+    let pool = context.get::<Pool>()?;
+
+    Ok(Document {
+        data: Post::public(pool).await?,
+    })
+}
+
+pub async fn create(mut context: Context, _: Next) -> Result<impl IntoResponse> {
+    let body: Document<NewPost> = context.read().json().await?;
+    let pool = context.get::<Pool>()?;
+
+    Ok(Document {
+        data: body.data.insert(pool).await?,
+    })
+}
+
+pub async fn show(context: Context, _: Next) -> Result<impl IntoResponse> {
+    let id = context.params().get::<i32>("id")?;
+    let pool = context.get::<Pool>()?;
+
+    Ok(Document {
+        data: Post::find(&pool, id).await?,
+    })
+}
+
+pub async fn update(context: Context, _: Next) -> Result<impl IntoResponse> {
+    let id = context.params().get::<i32>("id")?;
+    Ok(format!("Update Post: {}", id))
+}
+
+pub async fn destroy(context: Context, _: Next) -> Result<impl IntoResponse> {
+    let id = context.params().get::<i32>("id")?;
+    Ok(format!("Destroy Post: {}", id))
+}
