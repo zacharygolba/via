d9b1d152f9da61476c9a69e12baf9d3b59d6ee76
Zachary Golba <pino@hey.com>
2024-07-01 10:47:14 -0400

    Use a boxed HttpBody for inner the response body

--- src/response/response.rs
+++ src/response/response.rs
@@ -1,9 +1,15 @@
+use futures_util::{Stream, StreamExt};
 use http::{
-    header::{HeaderMap, HeaderValue},
+    header::{self, HeaderMap},
     StatusCode, Version,
 };
+use hyper::body::Bytes;
 
-use super::{body::Body, ResponseBuilder};
+use super::{
+    body::{Body, Frame},
+    ResponseBuilder,
+};
+use crate::Error;
 
 pub(crate) type OutgoingResponse = http::Response<Body>;
 
@@ -20,20 +26,34 @@ impl Response {
     where
         Body: From<T>,
     {
-        ResponseBuilder::with_body(Ok(body.into())).header(
-            http::header::CONTENT_TYPE,
-            HeaderValue::from_static("text/html; charset=utf-8"),
-        )
+        let body = Body::from(body);
+        let content_len = body.len();
+        let mut response = ResponseBuilder::with_body(Ok(body));
+
+        response = response.header(header::CONTENT_TYPE, "text/html; charset=utf-8");
+
+        if let Some(value) = content_len {
+            response = response.header(header::CONTENT_LENGTH, value)
+        }
+
+        response
     }
 
     pub fn text<T>(body: T) -> ResponseBuilder
     where
         Body: From<T>,
     {
-        ResponseBuilder::with_body(Ok(body.into())).header(
-            http::header::CONTENT_TYPE,
-            HeaderValue::from_static("text/plain; charset=utf-8"),
-        )
+        let body = Body::from(body);
+        let content_len = body.len();
+        let mut response = ResponseBuilder::with_body(Ok(body));
+
+        response = response.header(header::CONTENT_TYPE, "text/plain; charset=utf-8");
+
+        if let Some(value) = content_len {
+            response = response.header(header::CONTENT_LENGTH, value)
+        }
+
+        response
     }
 
     #[cfg(feature = "serde")]
@@ -43,14 +63,33 @@ impl Response {
     {
         use crate::Error;
 
-        let body_bytes = serde_json::to_vec(body)
+        let body = serde_json::to_vec(body)
             .map(Body::from)
             .map_err(Error::from);
+        let content_len = body.as_ref().map_or(None, Body::len);
+        let mut response = ResponseBuilder::with_body(body);
 
-        ResponseBuilder::with_body(body_bytes).header(
-            http::header::CONTENT_TYPE,
-            HeaderValue::from_static("application/json; charset=utf-8"),
-        )
+        response = response.header(header::CONTENT_TYPE, "application/json; charset=utf-8");
+
+        if let Some(value) = content_len {
+            response = response.header(header::CONTENT_LENGTH, value)
+        }
+
+        response
+    }
+
+    pub fn stream<T, D, E>(body: T) -> ResponseBuilder
+    where
+        T: Stream<Item = Result<D, E>> + Send + Sync + 'static,
+        Bytes: From<D>,
+        Error: From<E>,
+    {
+        let body = Body::stream(Box::pin(body.map(|result| match result {
+            Ok(data) => Ok(Frame::data(Bytes::from(data))),
+            Err(error) => Err(Error::from(error)),
+        })));
+
+        ResponseBuilder::with_body(Ok(body)).header(header::TRANSFER_ENCODING, "chunked")
     }
 
     pub fn body(&self) -> &Body {
