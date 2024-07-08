d1e981424c7b373286a10030204ad9161f61bda5
Zachary Golba <pino@hey.com>
2024-07-08 17:43:49 -0400

    Remove the unit route in the benchmark example

--- docs/examples/benchmarks/src/main.rs
+++ docs/examples/benchmarks/src/main.rs
@@ -8,10 +8,6 @@ async fn main() -> Result<()> {
         Response::text("Hello, world!").end()
     }));
 
-    app.at("/unit").respond(via::get(|_, _| async {
-        Response::builder().status(204).end()
-    }));
-
     app.listen(("127.0.0.1", 8080), |event| match event {
         Event::ConnectionError(error) | Event::UncaughtError(error) => {
             eprintln!("Error: {}", error);
