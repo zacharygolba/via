7323b8272f9799f758ea86ea7dce3ef633a39cbb
Zachary Golba <pino@hey.com>
2024-07-23 09:57:23 -0400

    Return a custom future type from AppService::call

--- src/app/service.rs
+++ src/app/service.rs
@@ -1,10 +1,16 @@
 use hyper::{body::Incoming, service::Service};
-use std::{convert::Infallible, sync::Arc};
+use std::{
+    convert::Infallible,
+    future::Future,
+    pin::Pin,
+    sync::Arc,
+    task::{Context, Poll},
+};
 
 use crate::{
     event::{Event, EventListener},
     middleware::BoxFuture,
-    response::Body,
+    response::{Body, Response},
     router::Router,
     App, Request, Result,
 };
@@ -13,6 +19,11 @@ pub struct AppService<State> {
     pub event_listener: EventListener,
     pub router: Arc<Router<State>>,
     pub state: Arc<State>,
+}
+
+pub struct FutureResponse {
+    event_listener: EventListener,
+    response_future: BoxFuture<Result<Response>>,
 }
 
 impl<State> AppService<State> {
@@ -43,33 +54,92 @@ where
     State: Send + Sync + 'static,
 {
     type Error = Infallible;
-    type Future = BoxFuture<Result<Self::Response, Self::Error>>;
+    type Future = FutureResponse;
     type Response = http::Response<Body>;
 
     fn call(&self, request: http::Request<Incoming>) -> Self::Future {
-        let mut request = {
-            let event_listener = self.event_listener.clone();
-            let state = Arc::clone(&self.state);
-
-            Request::new(request, state, event_listener)
-        };
-
-        let event_listener = self.event_listener.clone();
+        // Wrap the incoming request with `via::Request`.
+        let mut request = Request::new(
+            request,
+            Arc::clone(&self.state),
+            self.event_listener.clone(),
+        );
+        // Route the request to the appropriate middleware stack.
         let next = self.router.visit(&mut request);
 
-        Box::pin(async move {
-            let response = next.call(request).await.unwrap_or_else(|error| {
-                error.into_infallible_response(|error| {
-                    // If the error was not able to be converted into a response,
-                    // with the configured error format (i.e json), fallback to a
-                    // plain text response and propagate the reason why the error
-                    // could not be converted to the event listener so it can be
-                    // handled at the application level.
-                    event_listener.call(Event::UncaughtError(error));
-                })
-            });
+        Self::Future {
+            // Clone the event listener to pass to the future. This is used to
+            // notify the event listener of any recoverable errors that occur
+            // while processing the request.
+            event_listener: self.event_listener.clone(),
+            // Unwind the middleware stack and pass the returned future to our
+            // custom future type. We'll use this future to poll the middleware
+            // stack and generate a response for the request.
+            response_future: next.call(request),
+        }
+    }
+}
 
-            Ok(response.into_inner())
-        })
+impl FutureResponse {
+    fn project(
+        self: Pin<&mut Self>,
+    ) -> (
+        Pin<&EventListener>,
+        Pin<&mut dyn Future<Output = Result<Response>>>,
+    ) {
+        // Safety:
+        // This block is necessary because we need to project the fields of
+        // `FutureResponse` through the pinned reference.
+        unsafe {
+            // Get a mutable reference to the struct from the pinned box.
+            let this = self.get_unchecked_mut();
+            // `EventListener` is not pinned and does not contain any `Unpin`
+            // fields. Therefore, it is safe to pin it.
+            let event_listener = &this.event_listener;
+            // Get a mutable reference to the inner `Future` from the pinned
+            // box. This is safe because the box is pinned, so the `Future`
+            // cannot move.
+            let response_future = this.response_future.as_mut().get_unchecked_mut();
+
+            // Return the pinned references to `EventListener` and `Future`.
+            (
+                Pin::new_unchecked(event_listener),
+                Pin::new_unchecked(response_future),
+            )
+        }
+    }
+}
+
+impl Future for FutureResponse {
+    type Output = Result<http::Response<Body>, Infallible>;
+
+    fn poll(self: Pin<&mut Self>, context: &mut Context) -> Poll<Self::Output> {
+        let (event_listener, response_future) = self.project();
+
+        match response_future.poll(context) {
+            Poll::Pending => {
+                // The response is not ready yet. Return pending.
+                Poll::Pending
+            }
+            Poll::Ready(Err(error)) => {
+                // An error occurred in the middleware stack. We'll convert the
+                // error into a response with the configured error format. If we
+                // are unable to convert the error into a response, we'll fallback
+                // to a plain text response and propagate the error to the event
+                // listener so it can be handled at the application level.
+                let response = error.into_infallible_response(|error| {
+                    // Notify the event listener that an uncaught error occurred.
+                    event_listener.call(Event::UncaughtError(error));
+                });
+
+                // Unwrap the inner response from `via::Response` and return ready.
+                Poll::Ready(Ok(response.into_inner()))
+            }
+            Poll::Ready(Ok(response)) => {
+                // The response was successfully generated. Unwrap the inner
+                // response from `via::Response` and return ready.
+                Poll::Ready(Ok(response.into_inner()))
+            }
+        }
     }
 }
