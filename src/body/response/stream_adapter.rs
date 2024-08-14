c47fe2e1f18cfe6fd0f335d29c69a2a8065757f1
Zachary Golba <pino@hey.com>
2024-08-14 10:27:57 -0400

    refactor: use Into in generic trait bounds for StreamAdapter

--- src/body/response/stream_adapter.rs
+++ src/body/response/stream_adapter.rs
@@ -13,41 +13,41 @@ use crate::{Error, Result};
 /// `Result<Frame<Bytes>>`. This adapter allows for response bodies to be built
 /// from virtually any stream that yields data that can be converted into bytes.
 #[must_use = "streams do nothing unless polled"]
-pub struct StreamAdapter<T, D, E>
+pub struct StreamAdapter<S, D, E>
 where
-    T: Stream<Item = Result<D, E>> + Send,
-    Bytes: From<D>,
-    Error: From<E>,
+    S: Stream<Item = Result<D, E>> + Send,
+    D: Into<Bytes>,
+    E: Into<Error>,
 {
     /// The `Stream` that we are adapting to yield `Result<Frame<Bytes>>`.
-    stream: T,
+    stream: S,
 
     /// This field is used to mark `StreamAdapter` as `!Unpin`. This is necessary
-    /// because `T` may not be `Unpin` and we need to project the `stream` field
+    /// because `S` may not be `Unpin` and we need to project the `stream` field
     /// through a pinned reference to `Self` so it can be polled.
     _pin: PhantomPinned,
 }
 
-impl<T, D, E> StreamAdapter<T, D, E>
+impl<S, D, E> StreamAdapter<S, D, E>
 where
-    T: Stream<Item = Result<D, E>> + Send,
-    Bytes: From<D>,
-    Error: From<E>,
+    S: Stream<Item = Result<D, E>> + Send,
+    D: Into<Bytes>,
+    E: Into<Error>,
 {
-    pub(crate) fn new(stream: T) -> Self {
+    pub(crate) fn new(stream: S) -> Self {
         Self {
             stream,
             _pin: PhantomPinned,
         }
     }
 
-    fn project(self: Pin<&mut Self>) -> Pin<&mut T> {
+    fn project(self: Pin<&mut Self>) -> Pin<&mut S> {
         // Get a mutable reference to `self`.
         let this = unsafe {
             //
             // Safety:
             //
-            // `StreamAdapter` is generic of `T` where `T` is a `Stream` that may
+            // `StreamAdapter` is generic of `S` where `S` is a `Stream` that may
             // or may not be `!Unpin`. Due to this, we include a `_pin` field in
             // `StreamAdapter` to mark the struct as `!Unpin`. In order to get a
             // mutable reference to `self` from the pinned reference, we need to
@@ -66,7 +66,7 @@ where
             //
             // The `stream` field may or not be `!Unpin`. We have to use
             // `Pin::new_unchecked` to pin the reference `ptr`. This is safe as
-            // long as the type `T`  safely manages pinned references in it's
+            // long as the type `S` safely manages pinned references in it's
             // implementation since we are only using the pinned reference to
             // call the `poll_next` method.
             Pin::new_unchecked(ptr)
@@ -74,19 +74,19 @@ where
     }
 }
 
-impl<T, D, E> Stream for StreamAdapter<T, D, E>
+impl<S, D, E> Stream for StreamAdapter<S, D, E>
 where
-    T: Stream<Item = Result<D, E>> + Send,
-    Bytes: From<D>,
-    Error: From<E>,
+    S: Stream<Item = Result<D, E>> + Send,
+    D: Into<Bytes>,
+    E: Into<Error>,
 {
     type Item = Result<Frame<Bytes>>;
 
     fn poll_next(self: Pin<&mut Self>, context: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         self.project()
             .poll_next(context)
-            .map_ok(|data| Frame::data(Bytes::from(data)))
-            .map_err(Error::from)
+            .map_ok(|data| Frame::data(data.into()))
+            .map_err(|error| error.into())
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
