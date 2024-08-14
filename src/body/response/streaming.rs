b128bd4610381c5b29677315b3efb48b9ee3a6e7
Zachary Golba <pino@hey.com>
2024-08-14 12:27:25 -0400

    refactor: code cleanup in the Streaming response body type

--- src/body/response/streaming.rs
+++ src/body/response/streaming.rs
@@ -9,22 +9,28 @@ use std::{
 
 use crate::{body::size_hint, Error, Result};
 
+/// A type alias for the trait object that represents the stream of frames that
+/// compose the body of `Streaming`. This type alias exists to simply the type
+/// signatures in this module.
+type StreamBody = dyn Stream<Item = Result<Frame<Bytes>>> + Send;
+
 /// A streaming response body. Streaming bodies are useful when the body data is
 /// too large to be buffered in memory or when the each frame of the body needs
 /// to be processed as it is received.
 pub struct Streaming {
     /// A trait object that implements `Stream` that we'll poll to get the next
     /// frame of the response body in the `poll_frame` method.
-    stream: Box<dyn Stream<Item = Result<Frame<Bytes>>> + Send>,
+    stream: Box<StreamBody>,
 
     /// This field is used to mark `Streaming` as `!Unpin`. This is necessary
-    /// because `self.stream` may not be `Unpin` and we need to project the
-    /// `self.stream` through a pinned reference to `Self` so it can be
-    /// polled.
+    /// because `self.stream` may be `!Unpin` and we need to project `stream`
+    /// through a pinned reference to `Self` so it can be polled for the next
+    /// frame.
     _pin: PhantomPinned,
 }
 
 impl Streaming {
+    /// Creates a new `Streaming` response body with the provided `stream`.
     pub fn new<T>(stream: Box<T>) -> Self
     where
         T: Stream<Item = Result<Frame<Bytes>>> + Send + 'static,
@@ -35,21 +41,33 @@ impl Streaming {
         }
     }
 
-    fn project(self: Pin<&mut Self>) -> Pin<&mut (dyn Stream<Item = Result<Frame<Bytes>>> + Send)> {
+    /// Returns a pinned mutable reference to the `stream` field.
+    fn project(self: Pin<&mut Self>) -> Pin<&mut StreamBody> {
         // Get a mutable reference to `self`.
         let this = unsafe {
+            //
             // Safety:
-            // TODO: Add safety explanation.
+            //
+            // The `body` field may contain a `Streaming` response body which is
+            // not `Unpin`. We need to project the body field through a pinned
+            // reference to `Self` so that it can be polled in the `poll_frame`
+            // method. This is safe because no data is moved out of `self`.
             self.get_unchecked_mut()
         };
         // Get a mutable reference to the `stream` field.
-        let ptr = &mut *this.stream;
+        let stream = &mut *this.stream;
 
-        // Return a pinned reference to `stream` field.
+        // Return the pinned mutable reference to the `stream` field.
         unsafe {
+            //
             // Safety:
-            // TODO: Add safety explanation.
-            Pin::new_unchecked(ptr)
+            //
+            // The `stream` field is a trait object that implements `Stream` and
+            // may be `!Unpin`. Therefore we have to use `Pin::new_unchecked` to
+            // wrap the mutable reference to `stream` in a pinned reference. This
+            // is safe because we are not moving the value or any data owned by
+            // the `stream` field out of the pinned mutable reference.
+            Pin::new_unchecked(stream)
         }
     }
 }
