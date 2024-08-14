bc4e03d759f6851f06124c8015bcb82fd84fd2a2
Zachary Golba <pino@hey.com>
2024-08-14 13:07:00 -0400

    refactor: do not use composition for BodyDataStream

--- src/body/request/body_stream.rs
+++ src/body/request/body_stream.rs
@@ -6,25 +6,32 @@ use std::{
     task::{Context, Poll},
 };
 
-use crate::{body::size_hint, Error, Result};
+use crate::body::size_hint;
+use crate::Result;
 
+/// A stream of frames that compose the body and trailers of a request.
 #[must_use = "streams do nothing unless polled"]
 pub struct BodyStream {
     body: Box<Incoming>,
 }
 
 impl BodyStream {
+    /// Creates a new `BodyStream` with the provided request body.
     pub(crate) fn new(body: Box<Incoming>) -> Self {
         Self { body }
     }
 
+    /// Returns a pinned mutable reference to the `body` field. This method is
+    /// used to project the `body` field through a pinned reference to `Self`
+    /// so it can be polled for the next frame.
     fn project(self: Pin<&mut Self>) -> Pin<&mut Incoming> {
         // Get a mutable reference to self.
         let this = self.get_mut();
-        // Get a mutable reference to the `body` field.
+        // Get a mutable reference to the `body` field by dereferencing
+        // `Box<Incoming>` to `&mut Incoming`.
         let body = &mut *this.body;
 
-        // Return the pinned reference to the `body` field.
+        // Return the pinned mutable reference to the `body` field.
         Pin::new(body)
     }
 }
@@ -33,11 +40,15 @@ impl Stream for BodyStream {
     type Item = Result<Frame<Bytes>>;
 
     fn poll_next(self: Pin<&mut Self>, context: &mut Context) -> Poll<Option<Self::Item>> {
-        self.project().poll_frame(context).map_err(Error::from)
+        // Delegate the call to `self.body` to poll the next frame. If an error
+        // occurs while polling the body, map the error to a `via::Error`.
+        self.project()
+            .poll_frame(context)
+            .map_err(|error| error.into())
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        // Delegate the call to the stream to get a `SizeHint` and use the
+        // Delegate the call to `self.body` to get a `SizeHint` and use the
         // helper function to adapt the returned `SizeHint` to a tuple that
         // contains the lower and upper bound of the stream.
         size_hint::from_body_for_stream(&self.body)
