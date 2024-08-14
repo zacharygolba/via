1d50292a516aa8e00ebadbbdd914ef7024e483f1
Zachary Golba <pino@hey.com>
2024-08-14 11:39:06 -0400

    feat: add a FrameExt trait to the public api

--- src/body/mod.rs
+++ src/body/mod.rs
@@ -1,12 +1,13 @@
 mod either;
+mod frame;
 mod request;
 mod response;
 mod size_hint;
 
 pub use bytes::Bytes;
 pub use futures_core::Stream;
-pub use hyper::body::Frame;
 
+pub use frame::{Frame, FrameExt};
 pub use request::{BodyDataStream, BodyStream, ReadIntoBytes, ReadIntoString, RequestBody};
 pub use response::ResponseBody;
 
