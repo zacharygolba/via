c1ab38254cb43e50510be80229447d73bd470fd4
Zachary Golba <pino@hey.com>
2024-08-15 16:02:47 -0400

    refactor: keep the reexport of futures_core::Stream from via::body

--- src/body/mod.rs
+++ src/body/mod.rs
@@ -5,6 +5,7 @@ mod response;
 mod size_hint;
 
 pub use bytes::Bytes;
+pub use futures_core::Stream;
 
 pub use frame::{Frame, FrameExt};
 pub use request::{BodyDataStream, BodyStream, ReadIntoBytes, ReadIntoString, RequestBody};
