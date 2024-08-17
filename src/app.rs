4940e9742476148b131bb528d1610bd69257843c
Zachary Golba <pino@hey.com>
2024-08-17 13:38:36 -0400

    fix: put a semaphore in front of TcpListener

--- src/app.rs
+++ src/app.rs
@@ -1,25 +1,13 @@
-use hyper::body::Incoming;
-use hyper::server::conn::http1;
-use hyper::service::service_fn;
-use hyper_util::rt::{TokioIo, TokioTimer};
 use std::net::SocketAddr;
 use std::sync::Arc;
-use tokio::net::{TcpListener, ToSocketAddrs};
-use tokio::task;
+use tokio::net::ToSocketAddrs;
 
-use crate::body::Pollable;
 use crate::router::{Endpoint, Router};
-use crate::{Error, Middleware, Request};
-
-/// The request type used by our `hyper` service. This is the type that we will
-/// use to create a `via::Request` that will be passed to the middleware stack.
-type HttpRequest = http::Request<Incoming>;
-
-/// The response type used by our `hyper` service. This is the type that we will
-/// unwrap from the `via::Response` returned from the middleware stack.
-type HttpResponse = http::Response<Pollable>;
+use crate::server::{serve, TcpListener};
+use crate::{Error, Middleware};
 
 pub struct App<State> {
+    max_connections: Option<usize>,
     router: Router<State>,
     state: Arc<State>,
 }
@@ -30,85 +18,9 @@ where
     State: Send + Sync + 'static,
 {
     App {
+        max_connections: None,
         router: Router::new(),
         state: Arc::new(state),
-    }
-}
-
-async fn serve<State>(
-    state: Arc<State>,
-    router: Arc<Router<State>>,
-    tcp_listener: TcpListener,
-) -> Result<(), Error>
-where
-    State: Send + Sync + 'static,
-{
-    loop {
-        // Accept a new connection from the TCP listener.
-        let (stream, _) = tcp_listener.accept().await?;
-        // Pass the returned stream to the `TokioIo` wrapper to convert
-        // the stream into a tokio-compatible I/O stream.
-        let io = TokioIo::new(stream);
-
-        // Clone the `state` so it can be moved into the task.
-        let state = Arc::clone(&state);
-        // Clone the `router` so it can be moved into the task.
-        let router = Arc::clone(&router);
-
-        // Spawn a tokio task to serve multiple connections concurrently.
-        task::spawn(async move {
-            // Create a hyper service from the `serve_request` function.
-            let service = service_fn(|request| serve_request(&state, &router, request));
-
-            // Create a new connection for the configured HTTP version. For
-            // now we only support HTTP/1.1. This will be expanded to
-            // support HTTP/2 in the future.
-            let connection = http1::Builder::new()
-                .timer(TokioTimer::new())
-                .serve_connection(io, service);
-
-            if let Err(error) = connection.await {
-                //
-                // TODO:
-                //
-                // Replace eprintln with pretty_env_logger or something similar.
-                // We should also determine if this is how we want to handle
-                // connection errors long-term.
-                //
-                if cfg!(debug_assertions) {
-                    eprintln!("Error: {}", error);
-                }
-            }
-        });
-    }
-}
-
-/// Serves an incoming request by routing it to the corresponding middleware
-/// stack and returns a response.
-async fn serve_request<State>(
-    state: &Arc<State>,
-    router: &Arc<Router<State>>,
-    request: HttpRequest,
-) -> Result<HttpResponse, Error>
-where
-    State: Send + Sync + 'static,
-{
-    // Wrap the incoming request in with `via::Request`.
-    //
-    // Note:
-    //
-    // In the future, we may want to pass a reference to `state` to avoid having
-    // to incremet the ref count of the `Arc` unnecessarily.
-    //
-    let mut request = Request::new(request, Arc::clone(state));
-
-    // Route `request` to the corresponding middleware stack.
-    let next = router.visit(&mut request);
-
-    // Call the middleware stack and return a response.
-    match next.call(request).await {
-        Ok(response) => Ok(response.into_inner()),
-        Err(error) => Ok(error.into_response().into_inner()),
     }
 }
 
@@ -128,6 +40,11 @@ where
         self
     }
 
+    pub fn set_max_connections(&mut self, n: usize) -> &mut Self {
+        self.max_connections = Some(n);
+        self
+    }
+
     pub async fn listen<F, T>(self, address: T, listening: F) -> Result<(), Error>
     where
         F: FnOnce(&SocketAddr),
@@ -135,12 +52,11 @@ where
     {
         let state = self.state;
         let router = Arc::new(self.router);
-        let tcp_listener = TcpListener::bind(address).await?;
+        let listener = TcpListener::bind(address, self.max_connections).await?;
 
-        if let Ok(address) = tcp_listener.local_addr() {
-            // Notify the event listener that the server is ready to accept
-            // incoming connections at the address to which the TCP listener
-            // is bound.
+        if let Ok(address) = listener.local_address() {
+            // Call the listening callback with the address to which the TCP
+            // listener is bound.
             listening(&address);
         } else {
             // TODO:
@@ -152,6 +68,6 @@ where
         }
 
         // Serve incoming connections from the TCP listener.
-        serve(state, router, tcp_listener).await
+        serve(state, router, listener).await
     }
 }
---
+++ src/server/backoff.rs
@@ -0,0 +1,33 @@
+use std::time::Duration;
+
+pub struct Backoff {
+    base: u64,
+    exp: u32,
+    max: u64,
+}
+
+impl Backoff {
+    pub fn new(base_as_millis: u64, max_as_seconds: u64) -> Self {
+        Self {
+            base: base_as_millis,
+            exp: 0,
+            max: max_as_seconds * 1000,
+        }
+    }
+
+    pub fn next(&mut self) -> Duration {
+        let duration = self.base * (1 << self.exp);
+        let delay = if duration > self.max {
+            self.max
+        } else {
+            self.exp += 1;
+            duration
+        };
+
+        Duration::from_millis(delay)
+    }
+
+    pub fn reset(&mut self) {
+        self.exp = 0;
+    }
+}
