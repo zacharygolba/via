827fecc7daaf559a9267301389e2e61143a81455
Zachary Golba <pino@hey.com>
2024-08-21 10:55:13 -0400

    refactor(via): simplified use of pin in body::request

--- src/body/request/body_data_stream.rs
+++ src/body/request/body_data_stream.rs
@@ -1,36 +1,21 @@
 use bytes::Bytes;
 use futures_core::Stream;
-use hyper::body::{Body, Incoming};
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
-use crate::body::size_hint;
+use super::BodyStream;
 use crate::Result;
 
 /// A data stream of bytes that compose the body of a request.
 #[must_use = "streams do nothing unless polled"]
 pub struct BodyDataStream {
-    body: Box<Incoming>,
+    stream: BodyStream,
 }
 
 impl BodyDataStream {
-    /// Creates a new `BodyDataStream` with the provided request body.
-    pub(crate) fn new(body: Box<Incoming>) -> Self {
-        Self { body }
-    }
-
-    /// Returns a pinned mutable reference to the `body` field. This method is
-    /// used to project the `body` field through a pinned reference to `Self`
-    /// so it can be polled for the next frame.
-    fn project(self: Pin<&mut Self>) -> Pin<&mut Incoming> {
-        // Get a mutable reference to self.
-        let this = self.get_mut();
-        // Get a mutable reference to the `body` field by dereferencing
-        // `Box<Incoming>` to `&mut Incoming`.
-        let body = &mut *this.body;
-
-        // Return the pinned mutable reference to the `body` field.
-        Pin::new(body)
+    /// Creates a new `BodyDataStream` with the provided `BodyStream`.
+    pub(crate) fn new(stream: BodyStream) -> Self {
+        Self { stream }
     }
 }
 
@@ -38,14 +23,17 @@ impl Stream for BodyDataStream {
     type Item = Result<Bytes>;
 
     fn poll_next(self: Pin<&mut Self>, context: &mut Context) -> Poll<Option<Self::Item>> {
-        // Get a pinned mutable reference to `self.body`.
-        let mut body = self.project();
+        // Get a mutable reference to `Self`.
+        let this = self.get_mut();
+        // Get a mutable reference to the `stream` field.
+        let stream = &mut this.stream;
+        // Create a new `Pin` around the mtuable reference to the `stream` field.
+        let mut pin = Pin::new(stream);
 
         loop {
-            // Reborrow the pinned mutable reference to `self.body` and poll
-            // the stream for the next frame. If the stream is not ready,
-            // return early.
-            return match body.as_mut().poll_frame(context) {
+            // Reborrow the `pin` so we can poll the `stream` field for the next
+            // frame. If the stream is not ready, return early.
+            return match pin.as_mut().poll_next(context) {
                 // A frame was successfully polled from the stream. Attempt
                 // to pull the data out of the frame.
                 Poll::Ready(Some(Ok(frame))) => match frame.into_data() {
@@ -54,9 +42,9 @@ impl Stream for BodyDataStream {
                     // The frame is trailers. Ignore them and continue.
                     Err(_) => continue,
                 },
-                // An error occurred while polling the stream. Convert the
-                // error to a `via::Error` and return.
-                Poll::Ready(Some(Err(error))) => Poll::Ready(Some(Err(error.into()))),
+                // An error occurred while polling the stream. Return `Ready`
+                // with the error.
+                Poll::Ready(Some(Err(error))) => Poll::Ready(Some(Err(error))),
                 // The stream has been exhausted.
                 Poll::Ready(None) => Poll::Ready(None),
                 // Wait for the next frame.
@@ -66,9 +54,7 @@ impl Stream for BodyDataStream {
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        // Delegate the call to `self.body` to get a `SizeHint` and use the
-        // helper function to adapt the returned `SizeHint` to a tuple that
-        // contains the lower and upper bound of the stream.
-        size_hint::from_body_for_stream(&self.body)
+        // Return the size hint from the `BodyStream` at `self.stream`.
+        self.stream.size_hint()
     }
 }
--- src/body/request/body_stream.rs
+++ src/body/request/body_stream.rs
@@ -1,10 +1,8 @@
 use bytes::Bytes;
 use futures_core::Stream;
 use hyper::body::{Body, Frame, Incoming};
-use std::{
-    pin::Pin,
-    task::{Context, Poll},
-};
+use std::pin::Pin;
+use std::task::{Context, Poll};
 
 use crate::body::size_hint;
 use crate::Result;
@@ -20,29 +18,21 @@ impl BodyStream {
     pub(crate) fn new(body: Box<Incoming>) -> Self {
         Self { body }
     }
-
-    /// Returns a pinned mutable reference to the `body` field. This method is
-    /// used to project the `body` field through a pinned reference to `Self`
-    /// so it can be polled for the next frame.
-    fn project(self: Pin<&mut Self>) -> Pin<&mut Incoming> {
-        // Get a mutable reference to self.
-        let this = self.get_mut();
-        // Get a mutable reference to the `body` field by dereferencing
-        // `Box<Incoming>` to `&mut Incoming`.
-        let body = &mut *this.body;
-
-        // Return the pinned mutable reference to the `body` field.
-        Pin::new(body)
-    }
 }
 
 impl Stream for BodyStream {
     type Item = Result<Frame<Bytes>>;
 
     fn poll_next(self: Pin<&mut Self>, context: &mut Context) -> Poll<Option<Self::Item>> {
-        // Delegate the call to `self.body` to poll the next frame. If an error
-        // occurs while polling the body, map the error to a `via::Error`.
-        self.project()
+        // Get a mutable reference to `Self`.
+        let this = self.get_mut();
+        // Get a mutable reference to the `body` field.
+        let body = &mut this.body;
+
+        // Pin the mutable reference to the `body` field and poll it for the next
+        // frame. If an error occurs while polling `body`, map the error to a
+        // `via::Error`.
+        Pin::new(body)
             .poll_frame(context)
             .map_err(|error| error.into())
     }
