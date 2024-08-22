fd1deb7b539b2caeb5d0b8172b2e3064a752033c
Zachary Golba <pino@hey.com>
2024-08-22 12:25:01 -0400

    refactor(via): improvements to pin projects in via::body

--- src/body/response/buffered.rs
+++ src/body/response/buffered.rs
@@ -1,5 +1,6 @@
 use bytes::{Bytes, BytesMut};
 use hyper::body::{Body, Frame, SizeHint};
+use std::marker::PhantomPinned;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
@@ -8,12 +9,22 @@ use crate::{Error, Result};
 /// A buffered body that contains a `BytesMut` buffer. This variant is used
 /// when the entire body can be buffered in memory.
 pub struct Buffered {
-    data: Box<BytesMut>,
+    /// The buffer containing the body data.
+    data: BytesMut,
+
+    /// Buffered is marked as `!Unpin` because it parcitipates in an `Either`
+    /// enum that contains other types that are `!Unpin`. This is a safety
+    /// precaution to prevent the accidental movement of data out of the
+    /// `Buffered` type when it is a variant of an `Either` enum.
+    _pin: PhantomPinned,
 }
 
 impl Buffered {
-    pub fn new(data: Box<BytesMut>) -> Self {
-        Self { data }
+    pub fn new(data: BytesMut) -> Self {
+        Self {
+            data,
+            _pin: PhantomPinned,
+        }
     }
 
     pub fn is_empty(&self) -> bool {
@@ -22,6 +33,25 @@ impl Buffered {
 
     pub fn len(&self) -> usize {
         self.data.len()
+    }
+}
+
+impl Buffered {
+    /// Returns a pinned mutable reference to the `data` field.
+    fn project(self: Pin<&mut Self>) -> Pin<&mut BytesMut> {
+        unsafe {
+            //
+            // Safety:
+            //
+            // `BytesMut` is `Unpin`. However, it is used in the context of an
+            // `Either` enum that contain other types that are `!Unpin`. In order
+            // to prevent the accidental movement of data out of `self` or
+            // `self.data`, we mark `Buffered` as `!Unpin` and therefore need to
+            // use `Pin::map_unchecked_mut` to get a pinned reference to the
+            // `BytesMut` buffer.
+            //
+            self.map_unchecked_mut(|this| &mut this.data)
+        }
     }
 }
 
@@ -34,10 +64,10 @@ impl Body for Buffered {
         _: &mut Context<'_>,
     ) -> Poll<Option<Result<Frame<Self::Data>, Self::Error>>> {
         // Get a mutable reference to `self`.
-        let this = self.get_mut();
+        let mut buffer = self.project();
         // Get the length of the buffer. This is used to determine how
         // many bytes to copy from the buffer into the data frame.
-        let len = this.data.len();
+        let len = buffer.len();
 
         // Check if the buffer has any data.
         if len == 0 {
@@ -45,8 +75,10 @@ impl Body for Buffered {
             return Poll::Ready(None);
         }
 
-        // Copy the bytes from the buffer into an immutable `Bytes`.
-        let bytes = this.data.split_to(len).freeze();
+        // Copy the bytes from the buffer into an immutable `Bytes`. This is safe
+        // because `BytesMut` is only advancing an internal pointer rather than
+        // moving the bytes in memory.
+        let bytes = buffer.split_to(len).freeze();
         // Wrap the bytes we copied from buffer in a data frame.
         let frame = Frame::data(bytes);
 
