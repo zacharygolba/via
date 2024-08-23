16eb584c6d7e34dab50c4145f9cba96c9359bc5b
Zachary Golba <pino@hey.com>
2024-08-23 10:16:21 -0400

    feat(via): the size of Request and Next is the size of Box

--- src/request/request.rs
+++ src/request/request.rs
@@ -9,17 +9,21 @@ use super::params::{Param, Params, QueryParamValues};
 use crate::body::RequestBody;
 
 pub struct Request<State = ()> {
+    data: Box<RequestData<State>>,
+}
+
+struct RequestData<State> {
     /// The component parts of the underlying HTTP request.
-    parts: Box<Parts>,
+    parts: Parts,
 
     /// The shared application state associated with the request.
     state: Arc<State>,
 
-    /// The request's body.
-    body: RequestBody,
-
     /// The request's path and query parameters.
     params: Params,
+
+    /// The request's body.
+    body: RequestBody,
 }
 
 fn get_content_len(headers: &HeaderMap) -> Option<usize> {
@@ -33,12 +37,12 @@ impl<State> Request<State> {
     /// Returns a reference to a map that contains the headers associated with
     /// the request.
     pub fn headers(&self) -> &HeaderMap {
-        &self.parts.headers
+        &self.data.parts.headers
     }
 
     /// Returns a reference to the HTTP method associated with the request.
     pub fn method(&self) -> &Method {
-        &self.parts.method
+        &self.data.parts.method
     }
 
     /// Returns a convenient wrapper around an optional reference to the path
@@ -58,11 +62,11 @@ impl<State> Request<State> {
     /// ```
     pub fn param<'a>(&self, name: &'a str) -> Param<'_, 'a> {
         // Get the path of the request's uri.
-        let path = self.parts.uri.path();
+        let path = self.data.parts.uri.path();
         // Get an `Option<(usize, usize)>` that represents the start and end
         // offset of the path parameter with the provided `name` in the request's
         // uri.
-        let at = self.params.get_path_param(name);
+        let at = self.data.params.get_path_param(name);
 
         Param::new(at, name, path)
     }
@@ -70,9 +74,9 @@ impl<State> Request<State> {
     /// Returns a convenient wrapper around an optional references to the query
     /// parameters in the request's uri with the provided `name`.
     pub fn query<'a>(&mut self, name: &'a str) -> QueryParamValues<'_, 'a> {
-        self.parts.uri.query().map_or_else(
+        self.data.parts.uri.query().map_or_else(
             || QueryParamValues::empty(name),
-            |query| self.params.get_query_params(query, name),
+            |query| self.data.params.get_query_params(query, name),
         )
     }
 
@@ -80,29 +84,29 @@ impl<State> Request<State> {
     /// state that was passed as an argument to the [`via::app`](crate::app::app)
     /// function.
     pub fn state(&self) -> &Arc<State> {
-        &self.state
+        &self.data.state
     }
 
     /// Returns a reference to the uri associated with the request.
     pub fn uri(&self) -> &Uri {
-        &self.parts.uri
+        &self.data.parts.uri
     }
 
     /// Returns the HTTP version associated with the request.
     pub fn version(&self) -> Version {
-        self.parts.version
+        self.data.parts.version
     }
 
     /// Consumes the request and returns the body.
     pub fn into_body(self) -> RequestBody {
-        self.body
+        self.data.body
     }
 
     /// Consumes the request and returns a tuple containing the boxed component
     /// parts of the request, the body, and an `Arc` to the shared application
     /// state.
     pub fn into_parts(self) -> (Parts, RequestBody, Arc<State>) {
-        (*self.parts, self.body, self.state)
+        (self.data.parts, self.data.body, self.data.state)
     }
 }
 
@@ -110,10 +114,6 @@ impl<State> Request<State> {
     pub(crate) fn new(request: http::Request<Incoming>, params: Params, state: Arc<State>) -> Self {
         // Destructure the `http::Request` into its component parts.
         let (parts, body) = request.into_parts();
-        // Box the component parts of the request to keep the size of the request
-        // type small. This is important because the request type is passed by
-        // value to middleware and responder functions.
-        let parts = Box::new(parts);
         // Check if the request has a `Content-Length` header. If it does, wrap
         // the request body in a `RequestBody` with a known length. Otherwise,
         // wrap the request body in a `RequestBody` with an unknown length.
@@ -123,10 +123,12 @@ impl<State> Request<State> {
         };
 
         Self {
-            params,
-            parts,
-            state,
-            body,
+            data: Box::new(RequestData {
+                state,
+                params,
+                parts,
+                body,
+            }),
         }
     }
 }
@@ -136,10 +138,10 @@ impl<State> Debug for Request<State> {
         f.debug_struct("Request")
             .field("method", self.method())
             .field("uri", self.uri())
-            .field("params", &self.params)
+            .field("params", &self.data.params)
             .field("version", &self.version())
             .field("headers", self.headers())
-            .field("body", &self.body)
+            .field("body", &self.data.body)
             .finish()
     }
 }
