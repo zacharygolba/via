05fd5d9a4383c9cd4a75372be113ee6063bcc63c
Zachary Golba <pino@hey.com>
2024-08-23 13:11:17 -0400

    refactor(via): do not cache query params

--- src/request/params/query_param.rs
+++ src/request/params/query_param.rs
@@ -1,80 +1,58 @@
-use percent_encoding::percent_decode_str;
-use std::borrow::Cow;
-use std::slice::Iter;
+use std::iter::Peekable;
+use std::str::CharIndices;
 
-use super::Param;
+use super::{query_parser, Param};
 
-pub struct QueryParamValues<'a, 'b> {
+pub struct QueryParam<'a, 'b> {
     name: &'b str,
     query: &'a str,
-    values: Option<Vec<&'a (usize, usize)>>,
+    chars: Peekable<CharIndices<'a>>,
 }
 
-pub struct QueryParamValuesIter<'a> {
-    inner: Option<Iter<'a, &'a (usize, usize)>>,
-    query: &'a str,
-}
-
-impl<'a, 'b> QueryParamValues<'a, 'b> {
-    pub(crate) fn new(name: &'b str, query: &'a str, values: Vec<&'a (usize, usize)>) -> Self {
-        Self {
-            name,
-            query,
-            values: Some(values),
-        }
+impl QueryParam<'_, '_> {
+    #[allow(clippy::should_implement_trait)]
+    pub fn next(&mut self) -> Param {
+        Param::new(self.find_next(), self.name, self.query)
     }
 
-    pub(crate) fn empty(name: &'b str) -> Self {
-        Self {
-            name,
-            query: "",
-            values: None,
-        }
-    }
+    pub fn last(&mut self) -> Param {
+        let mut at = None;
 
-    pub fn get(&self, index: usize) -> Param<'a, 'b> {
-        let at = self
-            .values
-            .as_ref()
-            .and_then(|values| Some(*values.get(index)?));
+        while let Some(next) = self.find_next() {
+            at = Some(next);
+        }
 
         Param::new(at, self.name, self.query)
     }
 
-    pub fn first(&self) -> Param<'a, 'b> {
-        let at = self
-            .values
-            .as_ref()
-            .and_then(|values| Some(*values.first()?));
+    pub fn get(&mut self, index: usize) -> Param {
+        let mut at = None;
 
-        Param::new(at, self.name, self.query)
-    }
-
-    pub fn last(&self) -> Param<'a, 'b> {
-        let at = self
-            .values
-            .as_ref()
-            .and_then(|values| Some(*values.last()?));
-
-        Param::new(at, self.name, self.query)
-    }
-
-    pub fn iter(&self) -> QueryParamValuesIter {
-        QueryParamValuesIter {
-            inner: self.values.as_ref().map(|values| values.iter()),
-            query: self.query,
+        for _ in 0..index {
+            at = self.find_next();
         }
+
+        Param::new(at, self.name, self.query)
     }
 }
 
-impl<'a> Iterator for QueryParamValuesIter<'a> {
-    type Item = Cow<'a, str>;
+impl<'a, 'b> QueryParam<'a, 'b> {
+    pub(crate) fn new(name: &'b str, query: &'a str) -> Self {
+        let chars = query.char_indices().peekable();
 
-    fn next(&mut self) -> Option<Self::Item> {
-        let (start, end) = **self.inner.as_mut()?.next()?;
-        let raw_value = &self.query[start..end];
-        let decoder = percent_decode_str(raw_value);
+        Self { name, query, chars }
+    }
 
-        Some(decoder.decode_utf8_lossy())
+    fn find_next(&mut self) -> Option<(usize, usize)> {
+        let chars = &mut self.chars;
+        let query = self.query;
+
+        loop {
+            let (name, at) = query_parser::parse(chars, query)?;
+
+            if name == self.name {
+                return Some(at);
+            }
+        }
     }
 }
