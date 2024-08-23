db330048f92049cbcedbedf0f16723bf9236ff15
Zachary Golba <pino@hey.com>
2024-08-23 13:27:32 -0400

    refactor(via): use Vec instead of Box<VecDeque<_>> in Next

--- src/middleware/next.rs
+++ src/middleware/next.rs
@@ -1,24 +1,20 @@
-use std::collections::VecDeque;
-
 use super::ArcMiddleware;
 use crate::{BoxFuture, Error, Request, Response, Result};
 
 pub struct Next<State = ()> {
-    #[allow(clippy::box_collection)]
-    stack: Box<VecDeque<ArcMiddleware<State>>>,
+    stack: Vec<ArcMiddleware<State>>,
 }
 
 impl<State> Next<State>
 where
     State: Send + Sync + 'static,
 {
-    #[allow(clippy::box_collection)]
-    pub(crate) fn new(stack: Box<VecDeque<ArcMiddleware<State>>>) -> Self {
+    pub(crate) fn new(stack: Vec<ArcMiddleware<State>>) -> Self {
         Self { stack }
     }
 
     pub fn call(mut self, request: Request<State>) -> BoxFuture<Result<Response, Error>> {
-        if let Some(middleware) = self.stack.pop_front() {
+        if let Some(middleware) = self.stack.pop() {
             middleware.call(request, self)
         } else {
             Box::pin(async { Response::text("Not Found").status(404).finish() })
