8a44f1af10220da9ada639c2db3bd0623fbdf710
Zachary Golba <pino@hey.com>
2024-08-24 10:35:00 -0400

    fix(via): trait DecodeParam should be public

--- src/request/params/mod.rs
+++ src/request/params/mod.rs
@@ -4,8 +4,8 @@ mod path_params;
 mod query_param;
 mod query_parser;
 
+pub use decode::{DecodeParam, NoopDecoder, PercentDecoder};
 pub use param::Param;
 pub use query_param::{QueryParam, QueryParamIter};
 
-pub(crate) use decode::{DecodeParam, NoopDecoder, PercentDecoder};
 pub(crate) use path_params::PathParams;
