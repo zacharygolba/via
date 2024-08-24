d3cd204f9c372dd48c05edb893bd2ee60627285a
Zachary Golba <pino@hey.com>
2024-08-24 09:45:02 -0400

    refactor(via): remove type alias from body::response::Streaming

--- src/body/response/streaming.rs
+++ src/body/response/streaming.rs
@@ -7,18 +7,13 @@ use std::task::{Context, Poll};
 use crate::body::size_hint;
 use crate::{Error, Result};
 
-/// A type alias for the trait object that represents the stream of frames that
-/// compose the body of `Streaming`. This type alias exists to simply the type
-/// signatures in this module.
-type StreamBody = dyn Stream<Item = Result<Frame<Bytes>>> + Send;
-
 /// A streaming response body. Streaming bodies are useful when the body data is
 /// too large to be buffered in memory or when the each frame of the body needs
 /// to be processed as it is received.
 pub struct Streaming {
     /// A trait object that implements `Stream` that we'll poll to get the next
     /// frame of the response body in the `poll_frame` method.
-    stream: Box<StreamBody>,
+    stream: Box<dyn Stream<Item = Result<Frame<Bytes>>> + Send>,
 }
 
 impl Streaming {
@@ -33,7 +28,7 @@ impl Streaming {
 
 impl Streaming {
     /// Returns a pinned mutable reference to the `stream` field.
-    fn project(self: Pin<&mut Self>) -> Pin<&mut StreamBody> {
+    fn project(self: Pin<&mut Self>) -> Pin<&mut (dyn Stream<Item = Result<Frame<Bytes>>> + Send)> {
         unsafe {
             //
             // Safety:
