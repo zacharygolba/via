5c40a561754f6561fb3a3069d5c31dd5c01171f6
Zachary Golba <pino@hey.com>
2024-08-25 12:35:31 -0400

    feat(via): perf improvements to the QueryParser

--- src/request/params/query_parser.rs
+++ src/request/params/query_parser.rs
@@ -5,12 +5,56 @@ type QueryParserOutput<'a> = (Cow<'a, str>, Option<(usize, usize)>);
 
 pub struct QueryParser<'a> {
     input: &'a str,
-    offset: usize,
+    from: usize,
+}
+
+fn decode(input: &str) -> Cow<str> {
+    percent_decode_str(input).decode_utf8_lossy()
+}
+
+fn take_name(input: &str, from: usize) -> (usize, Option<Cow<str>>) {
+    // Get the start index of the name by taking the next index from input.
+    let result = take_while(input, from, |byte| byte == b'&').and_then(|start| {
+        let end = take_while(input, start, |byte| byte != b'=').unwrap_or(input.len());
+        let name = input.get(start..end).map(decode)?;
+
+        Some((end, name))
+    });
+
+    match result {
+        Some((offset, name)) => (offset, Some(name)),
+        None => (input.len(), None),
+    }
+}
+
+fn take_value(input: &str, from: usize) -> (usize, Option<(usize, usize)>) {
+    // Get the start index of the name by taking the next index from input.
+    let at = take_while(input, from, |byte| byte == b'=').map(|start| {
+        // Continue consuming the input until we reach the terminating ampersand.
+        match take_while(input, start, |byte| byte != b'&') {
+            // If we find the terminating ampersand, return the complete range.
+            Some(end) => (start, end),
+            // Otherwise, return the start index and the length of the input.
+            None => (start, input.len()),
+        }
+    });
+
+    match at {
+        Some((start, end)) => (end, Some((start, end))),
+        None => (input.len(), None),
+    }
+}
+
+fn take_while(input: &str, from: usize, f: impl Fn(u8) -> bool) -> Option<usize> {
+    let rest = input.get(from..)?;
+    let to = rest.bytes().position(|byte| !f(byte))?;
+
+    Some(from + to)
 }
 
 impl<'a> QueryParser<'a> {
     pub fn new(input: &'a str) -> Self {
-        Self { input, offset: 0 }
+        Self { input, from: 0 }
     }
 
     pub fn input(&self) -> &'a str {
@@ -22,75 +66,13 @@ impl<'a> Iterator for QueryParser<'a> {
     type Item = QueryParserOutput<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let (offset, param) = parse_param(self.input, self.offset);
+        let (start, name) = take_name(self.input, self.from);
+        let (end, at) = take_value(self.input, start);
 
-        self.offset = offset;
-        param
+        self.from = end;
+
+        name.zip(Some(at))
     }
-}
-
-fn decode(input: &str) -> Cow<str> {
-    percent_decode_str(input).decode_utf8_lossy()
-}
-
-fn has_latin_char_at(input: &str, at: usize) -> bool {
-    input.is_char_boundary(at) && input.is_char_boundary(at + 1)
-}
-
-fn parse_param(query: &str, from: usize) -> (usize, Option<QueryParserOutput>) {
-    let (offset, name) = parse_name(query, from);
-    let (offset, at) = parse_value(query, offset);
-    let param = name.map(|name| {
-        let (start, end) = at;
-        let at = if start < end {
-            Some((start, end))
-        } else {
-            None
-        };
-
-        (name, at)
-    });
-
-    (offset, param)
-}
-
-fn parse_name(input: &str, from: usize) -> (usize, Option<Cow<str>>) {
-    // Get the start index of the name by taking the next index from input.
-    let start = take_while(input, from, |byte| byte == b'&');
-    // Continue consuming the input until we reach the terminating equal sign.
-    let end = take_while(input, start, |byte| byte != b'=');
-    // Get the decoded version of the name if the range is valid.
-    let name = if start < end {
-        Some(decode(&input[start..end]))
-    } else {
-        None
-    };
-
-    (end, name)
-}
-
-fn parse_value(input: &str, from: usize) -> (usize, (usize, usize)) {
-    // Get the start index of the name by taking the next index from input.
-    let start = take_while(input, from, |byte| byte == b'=');
-    // Continue consuming the input until we reach the terminating ampersand.
-    let end = take_while(input, start, |byte| byte != b'&');
-
-    (end, (start, end))
-}
-
-fn take_while(input: &str, from: usize, f: impl Fn(u8) -> bool) -> usize {
-    input
-        .bytes()
-        .enumerate()
-        .skip(from)
-        .find_map(|(index, byte)| {
-            if has_latin_char_at(input, index) && f(byte) {
-                None
-            } else {
-                Some(index)
-            }
-        })
-        .unwrap_or(input.len())
 }
 
 #[cfg(test)]
@@ -124,41 +106,41 @@ mod tests {
         "query%C3%28=books&category=%C3%28", // Invalid UTF-8 sequence in key
     ];
 
-    fn get_expected_results() -> [Vec<(&'static str, (usize, usize))>; 21] {
+    fn get_expected_results() -> [Vec<(&'static str, Option<(usize, usize)>)>; 21] {
         [
             vec![
-                ("query", (6, 11)),
-                ("category", (21, 28)),
-                ("sort", (34, 37)),
+                ("query", Some((6, 11))),
+                ("category", Some((21, 28))),
+                ("sort", Some((34, 37))),
             ],
-            vec![("query", (6, 19)), ("category", (29, 38))],
+            vec![("query", Some((6, 19))), ("category", Some((29, 38)))],
             vec![
-                ("category", (9, 14)),
-                ("category", (24, 35)),
-                ("category", (45, 53)),
+                ("category", Some((9, 14))),
+                ("category", Some((24, 35))),
+                ("category", Some((45, 53))),
             ],
-            vec![("query", (6, 11))],
-            vec![("query", (6, 22)), ("category", (32, 36))],
-            vec![("query", (6, 11)), ("filter", (19, 45))],
+            vec![("query", Some((6, 11))), ("category", None)],
+            vec![("query", Some((6, 22))), ("category", Some((32, 36)))],
+            vec![("query", Some((6, 11))), ("filter", Some((19, 45)))],
             vec![
-                ("items[]", (8, 12)),
-                ("items[]", (21, 24)),
-                ("items[]", (33, 41)),
+                ("items[]", Some((8, 12))),
+                ("items[]", Some((21, 24))),
+                ("items[]", Some((33, 41))),
             ],
             vec![],
-            vec![("data", (5, 47))],
-            vec![("query", (6, 11)), ("category", (21, 37))],
-            vec![("query", (6, 11)), ("category", (22, 29))],
-            vec![("query", (7, 12)), ("category", (22, 29))],
-            vec![("query", (6, 11))],
-            vec![("query", (6, 11))],
-            vec![("query", (6, 11)), ("category", (21, 28))],
-            vec![("query", (8, 13)), ("category", (25, 32))],
-            vec![("query", (8, 13)), ("category", (25, 32))],
-            vec![("query", (6, 11)), ("category", (23, 32))],
-            vec![("query", (6, 11)), ("category", (21, 30))],
-            vec![("query", (6, 11)), ("category", (21, 27))],
-            vec![("query�(", (12, 17)), ("category", (27, 33))],
+            vec![("data", Some((5, 47)))],
+            vec![("query", Some((6, 11))), ("category", Some((21, 37)))],
+            vec![("query", Some((6, 11))), ("category", Some((22, 29)))],
+            vec![("query", Some((7, 12))), ("category", Some((22, 29)))],
+            vec![("query", Some((6, 11))), ("category", None)],
+            vec![("query", Some((6, 11))), ("", Some((13, 20)))],
+            vec![("query", Some((6, 11))), ("category", Some((21, 28)))],
+            vec![("query", Some((8, 13))), ("category", Some((25, 32)))],
+            vec![("query", Some((8, 13))), ("category", Some((25, 32)))],
+            vec![("query", Some((6, 11))), ("category", Some((23, 32)))],
+            vec![("query", Some((6, 11))), ("category", Some((21, 30)))],
+            vec![("query", Some((6, 11))), ("category", Some((21, 27)))],
+            vec![("query�(", Some((12, 17))), ("category", Some((27, 33)))],
         ]
     }
 
@@ -167,30 +149,34 @@ mod tests {
         let expected_results = get_expected_results();
 
         for (expected_result_index, query) in QUERY_STRINGS.iter().enumerate() {
+            let mut actual_result = vec![];
             let expected_result = &expected_results[expected_result_index];
 
-            for (entry_index, (name, at)) in QueryParser::new(query).enumerate() {
-                let at = match at {
-                    Some((start, end)) => (start, end),
-                    None => continue,
-                };
+            actual_result.extend(QueryParser::new(query));
 
-                let (expected_name, expected_at) = expected_result[entry_index];
-                let expected_value = query
-                    .get(expected_at.0..expected_at.1)
-                    .unwrap_or_else(|| &query[expected_at.0..]);
-                let value = query.get(at.0..at.1).unwrap_or_else(|| &query[at.0..]);
+            assert_eq!(
+                actual_result.len(),
+                expected_result.len(),
+                "Expected {} to have {} entries. Got {} instead.\n\n{}\n\n",
+                expected_result_index,
+                expected_result.len(),
+                actual_result.len(),
+                query
+            );
+
+            for (entry_index, (name, at)) in actual_result.into_iter().enumerate() {
+                let expect = expected_result[entry_index];
 
                 assert_eq!(
-                    name, expected_name,
+                    name, expect.0,
                     "Expected name at index [{}, {}] to be {}. Got {} instead.",
-                    expected_result_index, entry_index, expected_name, name
+                    expected_result_index, entry_index, expect.0, name
                 );
 
                 assert_eq!(
-                    value, expected_value,
-                    "Expected range at index [{}, {}] to be {}. Got {} instead.",
-                    expected_result_index, entry_index, expected_value, value
+                    at, expect.1,
+                    "Expected range at index [{}, {}] to be {:?}. Got {:?} instead.",
+                    expected_result_index, entry_index, expect.1, at
                 );
             }
         }
