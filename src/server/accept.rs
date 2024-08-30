7fccf5fd2a4cd74447202998c8649fb0e41a4852
Zachary Golba <pino@hey.com>
2024-08-30 14:34:50 -0400

    fix(via): allocate 2 semaphore permits per connection

--- src/server/accept.rs
+++ src/server/accept.rs
@@ -1,5 +1,6 @@
 use std::net::SocketAddr;
 use std::sync::Arc;
+use std::time::Duration;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::{OwnedSemaphorePermit, Semaphore, TryAcquireError};
 use tokio::time;
@@ -7,16 +8,37 @@ use tokio::time;
 use super::Backoff;
 use crate::Error;
 
+const ONE_SECOND: Duration = Duration::from_secs(1);
+
+/// Sleeps for provided `duration` or until the `wake` fn returns `true`.
+async fn sleep(duration: Duration, wake: impl Fn() -> bool) {
+    let mut remaining = duration;
+
+    if remaining.as_secs() == 0 {
+        time::sleep(remaining).await;
+        return;
+    }
+
+    while !wake() && !remaining.is_zero() {
+        if remaining.as_secs() > 0 {
+            time::sleep(ONE_SECOND).await;
+            remaining -= ONE_SECOND;
+        } else {
+            time::sleep(remaining).await;
+            remaining = Duration::ZERO;
+        }
+    }
+}
+
 pub async fn accept(
     backoff: &mut Backoff,
     listener: &TcpListener,
     semaphore: &Arc<Semaphore>,
 ) -> Result<(TcpStream, SocketAddr, OwnedSemaphorePermit), Error> {
     loop {
-        let permit = match Arc::clone(semaphore).try_acquire_owned() {
+        let permit = match Arc::clone(semaphore).try_acquire_many_owned(2) {
             Err(TryAcquireError::NoPermits) => {
-                let delay = backoff.next();
-                time::sleep(delay).await;
+                sleep(backoff.next(), || semaphore.available_permits() > 3).await;
                 continue;
             }
             Err(TryAcquireError::Closed) => {
