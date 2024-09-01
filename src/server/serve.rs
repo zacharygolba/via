91b3d903b4c7ff72141f4271bba5b62deb975a02
Zachary Golba <pino@hey.com>
2024-09-01 13:15:26 -0400

    feat(via): wait for connections to close before exit

--- src/server/serve.rs
+++ src/server/serve.rs
@@ -1,9 +1,11 @@
 use hyper::server::conn::http1;
 use hyper_util::rt::{TokioIo, TokioTimer};
+use std::net::SocketAddr;
+use std::pin::pin;
 use std::sync::Arc;
 use std::time::Duration;
-use tokio::net::TcpListener;
-use tokio::sync::Semaphore;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::{watch, OwnedSemaphorePermit, Semaphore};
 use tokio::task::{self, JoinHandle};
 
 use super::service::Service;
@@ -24,6 +26,10 @@ where
     // periodically check if any of the tasks have finished and remove them.
     let mut handles: Vec<JoinHandle<()>> = Vec::new();
 
+    let mut signal = pin!(tokio::signal::ctrl_c());
+
+    let (tx, rx) = watch::channel(false);
+
     // Create a semaphore with a number of permits equal to the maximum number
     // of connections that the server can handle concurrently. If the maximum
     // number of connections is reached, we'll wait until a permit is available
@@ -33,6 +39,8 @@ where
     loop {
         // Remove any handles that have finished.
         handles.retain(|handle| !handle.is_finished());
+
+        let mut rx = rx.clone();
 
         // Create a hyper service to serve the incoming connection. We'll move
         // the service into a tokio task to distribute the load across multiple
@@ -44,47 +52,98 @@ where
             Service::new(router, state, response_timeout)
         };
 
-        // Acquire a permit from the semaphore.
-        let permit = semaphore.clone().acquire_many_owned(2).await?;
+        tokio::select! {
+            result = accept(&listener, &semaphore) => {
+                let (permit, (stream, _addr)) = match result {
+                    Ok(accepted) => accepted,
+                    Err(_) => {
+                        //
+                        // TODO:
+                        //
+                        // Include tracing information about why the connection could not
+                        // be accepted.
+                        //
+                        continue;
+                    }
+                };
 
-        // Attempt to accept a new connection from the TCP listener.
-        let (stream, _addr) = match listener.accept().await {
-            Ok(accepted) => accepted,
-            Err(_) => {
-                drop(permit);
-                //
-                // TODO:
-                //
-                // Include tracing information about why the connection could not
-                // be accepted.
-                //
-                continue;
+                // Create a new connection for the configured HTTP version. For
+                // now we only support HTTP/1.1. This will be expanded to
+                // support HTTP/2 in the future.
+                let connection = http1::Builder::new()
+                    .timer(TokioTimer::new())
+                    .serve_connection(TokioIo::new(stream), service);
+
+                // Spawn a tokio task to serve the connection in a separate thread.
+                handles.push(task::spawn(async move {
+                    let mut connection = pin!(connection);
+
+                    tokio::select! {
+                        result = connection.as_mut() => {
+                            drop(permit);
+
+                            if let Err(error) = result {
+                                //
+                                // TODO:
+                                //
+                                // Replace eprintln with pretty_env_logger or something similar.
+                                // We should also determine if this is how we want to handle
+                                // connection errors long-term.
+                                //
+                                if cfg!(debug_assertions) {
+                                    eprintln!("Error: {}", error);
+                                }
+                            }
+                        }
+                        _ = rx.changed() => {
+                            drop(permit);
+                            connection.graceful_shutdown();
+                        }
+                    }
+                }));
             }
-        };
-
-        // Create a new connection for the configured HTTP version. For
-        // now we only support HTTP/1.1. This will be expanded to
-        // support HTTP/2 in the future.
-        let connection = http1::Builder::new()
-            .timer(TokioTimer::new())
-            .serve_connection(TokioIo::new(stream), service);
-
-        // Spawn a tokio task to serve the connection in a separate thread.
-        handles.push(task::spawn(async {
-            if let Err(error) = connection.await {
-                //
-                // TODO:
-                //
-                // Replace eprintln with pretty_env_logger or something similar.
-                // We should also determine if this is how we want to handle
-                // connection errors long-term.
-                //
-                if cfg!(debug_assertions) {
-                    eprintln!("Error: {}", error);
-                }
+            _ = signal.as_mut() => {
+                tx.send(true)?;
+                break;
             }
+        }
+    }
 
+    let shutdown = async {
+        while !handles.is_empty() {
+            handles.retain(|handle| !handle.is_finished());
+            tokio::time::sleep(Duration::from_secs(1)).await;
+            println!("Waiting for {} connections to close...", handles.len());
+        }
+    };
+
+    tokio::select! {
+        _ = shutdown => Ok(()),
+        _ = tokio::time::sleep(Duration::from_secs(30)) => {
+            Err(Error::new("process exited before all connections were closed.".to_string()))
+        }
+    }
+}
+
+async fn accept(
+    listener: &TcpListener,
+    semaphore: &Arc<Semaphore>,
+) -> Result<(OwnedSemaphorePermit, (TcpStream, SocketAddr)), Error> {
+    // Acquire a permit from the semaphore.
+    let permit = semaphore.clone().acquire_many_owned(2).await?;
+
+    // Attempt to accept a new connection from the TCP listener.
+    match listener.accept().await {
+        Ok(accepted) => Ok((permit, accepted)),
+        Err(error) => {
             drop(permit);
-        }));
+            //
+            // TODO:
+            //
+            // Include tracing information about why the connection could not
+            // be accepted.
+            //
+            Err(error.into())
+        }
     }
 }
