363d37b8490661b168e00514a0111c288f1ba20d
Zachary Golba <pino@hey.com>
2024-09-12 15:06:07 -0400

    fix(via): response body should be !Unpin and favor the stack

--- src/body/buffered.rs
+++ src/body/buffered.rs
@@ -1,6 +1,7 @@
-use bytes::Bytes;
+use bytes::{Bytes, BytesMut};
 use http_body::{Body, Frame, SizeHint};
 use std::fmt::{self, Debug, Formatter};
+use std::marker::PhantomPinned;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
@@ -13,12 +14,20 @@ const MAX_FRAME_LEN: usize = 16384; // 16KB
 #[must_use = "streams do nothing unless polled"]
 pub struct BufferedBody {
     /// The buffer containing the body data.
-    buf: Bytes,
+    buf: BytesMut,
+
+    /// A marker type indicating that the `BufferedBody` is pinned in memory.
+    _pin: PhantomPinned,
 }
 
 impl BufferedBody {
-    pub fn new(buf: Bytes) -> Self {
-        Self { buf }
+    pub fn new(data: &[u8]) -> Self {
+        let unique = Bytes::copy_from_slice(data);
+
+        Self {
+            buf: BytesMut::from(unique),
+            _pin: PhantomPinned,
+        }
     }
 
     pub fn is_empty(&self) -> bool {
@@ -30,6 +39,25 @@ impl BufferedBody {
     }
 }
 
+impl BufferedBody {
+    fn project(self: Pin<&mut Self>) -> Pin<&mut BytesMut> {
+        //
+        // Safety:
+        //
+        // No data is moved out of self or the `buf` field in the APIs that
+        // we use in the implementation of Body::poll_frame for BufferedBody.
+        // Furthermore, the bytes crate is frozen to a version that we know does
+        // not move the buffer in any of it's APIs.
+        //
+        // It is integral to the safety of this pin projection that future
+        // upgrades of the bytes crate are audited to ensure that the internal
+        // buffer of BytesMut is not moved in any of the APIs that are called in
+        // the implementation of Body::poll_frame.
+        //
+        unsafe { Pin::map_unchecked_mut(self, |this| &mut this.buf) }
+    }
+}
+
 impl Body for BufferedBody {
     type Data = Bytes;
     type Error = Error;
@@ -38,10 +66,8 @@ impl Body for BufferedBody {
         self: Pin<&mut Self>,
         _: &mut Context<'_>,
     ) -> Poll<Option<Result<Frame<Self::Data>, Self::Error>>> {
-        // Get a mutable reference to `self`.
-        let this = self.get_mut();
         // Get a mutable reference to the `buf` field.
-        let buf = &mut this.buf;
+        let mut buf = self.project();
 
         // Get the number of bytes to read from `buffer` for the current frame.
         // We read a maximum of 16KB per frame from `buffer`.
@@ -56,7 +82,7 @@ impl Body for BufferedBody {
         // Split the buffer at the frame length. This will give us an owned
         // view of the frame at 0..frame_len and advance the buffer to the
         // offset of the next frame.
-        let data = buf.split_to(len);
+        let data = buf.split_to(len).freeze();
         // Wrap the bytes we copied from buffer in a data frame.
         let frame = Frame::data(data);
 
@@ -81,54 +107,42 @@ impl Body for BufferedBody {
 
 impl Debug for BufferedBody {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        let len = self.buf.len();
-        f.debug_struct("Buffer").field("len", &len).finish()
+        f.debug_struct("BufferedBody").finish()
     }
 }
 
 impl Default for BufferedBody {
     fn default() -> Self {
-        Self::new(Bytes::new())
+        Self::new(&[])
     }
 }
 
 impl From<Bytes> for BufferedBody {
     fn from(bytes: Bytes) -> Self {
-        let buf = Bytes::copy_from_slice(&bytes);
-        Self { buf }
+        Self::new(&bytes)
     }
 }
 
 impl From<Vec<u8>> for BufferedBody {
     fn from(vec: Vec<u8>) -> Self {
-        let slice = vec.as_slice();
-        let buf = Bytes::copy_from_slice(slice);
-
-        Self { buf }
+        Self::new(vec.as_slice())
     }
 }
 
 impl From<&'static [u8]> for BufferedBody {
     fn from(slice: &'static [u8]) -> Self {
-        let buf = Bytes::copy_from_slice(slice);
-        Self { buf }
+        Self::new(slice)
     }
 }
 
 impl From<String> for BufferedBody {
     fn from(string: String) -> Self {
-        let slice = string.as_bytes();
-        let buf = Bytes::copy_from_slice(slice);
-
-        Self { buf }
+        Self::new(string.as_bytes())
     }
 }
 
 impl From<&'static str> for BufferedBody {
     fn from(slice: &'static str) -> Self {
-        let slice = slice.as_bytes();
-        let buf = Bytes::copy_from_slice(slice);
-
-        Self { buf }
+        Self::new(slice.as_bytes())
     }
 }
