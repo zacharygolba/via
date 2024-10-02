6879e8288ba3fa62f1e1d4bad48db314dfa13a33
Zachary Golba <pino@hey.com>
2024-10-02 13:16:27 -0400

    fix(via-router): return a copy of Param from Visit::next

--- crates/via-router/src/iter.rs
+++ crates/via-router/src/iter.rs
@@ -4,29 +4,34 @@ use crate::path::Param;
 use crate::routes::RouteStore;
 use crate::visitor::Visited;
 
-/// An iterator over the routes that match a given path.
+/// An iterator over the nodes that match a uri path.
 ///
 pub struct Visit<'a, T> {
     store: &'a RouteStore<T>,
     iter: IntoIter<Visited>,
 }
 
+/// A matched node in the route tree.
+///
+/// Contains a reference to the route associated with the node and additional
+/// metadata about the match.
+///
 #[derive(Debug)]
 pub struct Found<'a, T> {
     /// True if there were no more segments to match against the children of the
     /// matched node. Otherwise, false.
     ///
-    pub is_leaf_match: bool,
-
-    /// A reference to the name of the dynamic parameter that matched the path
-    /// segment.
-    ///
-    pub param_name: Option<&'a Param>,
+    pub is_leaf: bool,
 
     /// A reference to the route referenced by the node that matched the path
     /// segment.
     ///
     pub route: Option<&'a T>,
+
+    /// A reference to the name of the dynamic parameter that matched the path
+    /// segment.
+    ///
+    pub param: Option<Param>,
 
     /// An array containing the start and end index of the path segment that
     /// matched the node containing `route`.
@@ -49,9 +54,9 @@ impl<'a, T> Iterator for Visit<'a, T> {
         let node = store.get(visited.key);
 
         Some(Found {
-            is_leaf_match: visited.is_leaf_match,
-            param_name: node.param(),
+            is_leaf: visited.is_leaf,
             route: node.route.and_then(|key| store.route(key)),
+            param: node.param().cloned(),
             at: visited.at,
         })
     }
@@ -64,9 +69,9 @@ impl<'a, T> DoubleEndedIterator for Visit<'a, T> {
         let node = store.get(visited.key);
 
         Some(Found {
-            is_leaf_match: visited.is_leaf_match,
-            param_name: node.param(),
+            is_leaf: visited.is_leaf,
             route: node.route.and_then(|key| store.route(key)),
+            param: node.param().cloned(),
             at: visited.at,
         })
     }
