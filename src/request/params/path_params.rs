b848a3019a6670008063e1e2d3ab5f1f6c541631
Zachary Golba <pino@hey.com>
2024-10-05 14:24:31 -0400

    refactor(via): use the new match range api

--- src/request/params/path_params.rs
+++ src/request/params/path_params.rs
@@ -1,26 +1,26 @@
 use std::fmt::{self, Debug, Formatter};
-use via_router::Param;
+use via_router::{Param, Span};
 
 pub struct PathParams {
-    data: Vec<(Param, [usize; 2])>,
+    data: Vec<(Param, Span)>,
 }
 
 impl PathParams {
-    pub fn new(data: Vec<(Param, [usize; 2])>) -> Self {
+    pub fn new(data: Vec<(Param, Span)>) -> Self {
         Self { data }
     }
 
-    pub fn get(&self, predicate: &str) -> Option<&[usize; 2]> {
+    pub fn get(&self, predicate: &str) -> Option<(usize, usize)> {
         self.data.iter().find_map(|(name, at)| {
             if predicate == name.as_str() {
-                Some(at)
+                Some((at.start(), at.end()))
             } else {
                 None
             }
         })
     }
 
-    pub fn push(&mut self, param: (Param, [usize; 2])) {
+    pub fn push(&mut self, param: (Param, Span)) {
         self.data.push(param);
     }
 }
