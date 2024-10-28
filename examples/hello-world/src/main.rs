a63852345b5e473744cbab5f2f536113eddf1999
Zachary Golba <pino@hey.com>
2024-10-28 12:59:54 -0400

    docs: sync readme example with ./examples/hello-world

--- examples/hello-world/src/main.rs
+++ examples/hello-world/src/main.rs
@@ -11,26 +11,11 @@ async fn hello(request: Request, _: Next) -> via::Result<String> {
 
 #[tokio::main]
 async fn main() -> Result<(), BoxError> {
-    // Create a new app by calling the `via::app` function.
+    // Create a new application.
     let mut app = via::new(());
-    //                     ^^
-    // Shared state can be passed to the app by passing a value to the
-    // `via::app` function. Check out the shared-state example for more
-    // information.
 
-    // Add our hello responder to the endpoint /hello/:name. Middleware that is
-    // added to an endpoint with `.respond()` will only run if a request's path
-    // matches the path of the endpoint exactly.
+    // Define a route that listens on /hello/:name.
     app.at("/hello/:name").respond(via::get(hello));
-    //             ^^^^^           ^^^^^^^^
-    // When defining an endpoint with a colon prefix in a path segment, the
-    // path segment is treated as dynamic and will match any value up to the
-    // next path segment or the end of the request uri. A reference to the path
-    // path parameter will be available in the requests params under the name
-    // that immediately follows the colon in the path segment (i.e "name").
-    //
-    // You can specify the HTTP method that middleware should accept with the
-    // helper functions at the top-level of the `via` crate.
 
     // Start the server.
     Server::new(app).listen(("127.0.0.1", 8080)).await
