1bee72798ed2971b13fb16b80b378272859d59fd
Zachary Golba <pino@hey.com>
2025-03-03 17:15:47 -0500

    feat(via): support etag and last_modified headers in File

--- src/response/file.rs
+++ src/response/file.rs
@@ -1,13 +1,23 @@
-use http::header::{CONTENT_LENGTH, CONTENT_TYPE};
+use http::header::{CONTENT_LENGTH, CONTENT_TYPE, ETAG, LAST_MODIFIED};
+use httpdate::HttpDate;
+use std::fs::Metadata;
 use std::io::{self, ErrorKind};
 use std::path::{Path, PathBuf};
 use tokio::fs;
 
 use super::Response;
-use crate::middleware;
-use crate::Error;
+use crate::{middleware, Error};
 
-fn respond_with_io_error(error: io::Error) -> Error {
+/// Serve a single file from disk.
+///
+pub struct File {
+    path: PathBuf,
+    etag: Option<fn(&Metadata) -> Result<String, Error>>,
+    mime_type: Option<String>,
+    with_last_modified: bool,
+}
+
+fn wrap_io_error(error: io::Error) -> Error {
     let kind = error.kind();
     let source = Box::new(error);
 
@@ -18,18 +28,20 @@ fn respond_with_io_error(error: io::Error) -> Error {
     }
 }
 
-/// Serve a single file from disk.
-///
-pub struct File {
-    path: PathBuf,
-    mime_type: Option<String>,
-}
-
 impl File {
     pub fn open(path: impl AsRef<Path>) -> Self {
         Self {
             path: path.as_ref().to_path_buf(),
+            etag: None,
             mime_type: None,
+            with_last_modified: false,
+        }
+    }
+
+    pub fn etag(self, f: fn(&Metadata) -> Result<String, Error>) -> Self {
+        Self {
+            etag: Some(f),
+            ..self
         }
     }
 
@@ -40,12 +52,37 @@ impl File {
         }
     }
 
+    pub fn with_last_modified(self) -> Self {
+        Self {
+            with_last_modified: true,
+            ..self
+        }
+    }
+
     pub async fn serve(self) -> middleware::Result {
-        let data = fs::read(&self.path).await.map_err(respond_with_io_error)?;
+        let path = self.path.as_path();
+        let data = fs::read(path).await.map_err(wrap_io_error)?;
+        let meta = if self.etag.is_some() || self.with_last_modified {
+            Some(fs::metadata(path).await.map_err(wrap_io_error)?)
+        } else {
+            None
+        };
+
         let mut response = Response::build().header(CONTENT_LENGTH, data.len());
 
-        if let Some(mime_type) = &self.mime_type {
+        if let Some(mime_type) = self.mime_type.as_ref() {
             response = response.header(CONTENT_TYPE, mime_type);
+        }
+
+        if let Some(metadata) = meta.as_ref() {
+            if self.with_last_modified {
+                let last_modified = HttpDate::from(metadata.modified()?);
+                response = response.header(LAST_MODIFIED, last_modified.to_string());
+            }
+
+            if let Some(f) = self.etag.as_ref() {
+                response = response.header(ETAG, f(metadata)?);
+            }
         }
 
         response.body(data.into())
