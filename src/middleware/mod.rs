cd97505f7fcae539ae1923d3c34f0b8b42455f0f
Zachary Golba <pino@hey.com>
2025-03-03 13:50:06 -0500

    feat(via): basic file system support + favicon middleware

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -3,9 +3,15 @@ pub mod error_boundary;
 
 pub(crate) mod accept_method;
 
+#[cfg(feature = "fs")]
+mod favicon;
+
 mod middleware;
 mod next;
 mod timeout;
+
+#[cfg(feature = "fs")]
+pub use favicon::favicon;
 
 pub use middleware::{Middleware, Result};
 pub use next::Next;
