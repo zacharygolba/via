6eb304f73cf747b3c2ca1edd352b5c4ad98c7796
Zachary Golba <pino@hey.com>
2025-03-04 13:09:41 -0500

    refactor(via): remove the favicon middleware

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -3,15 +3,9 @@ pub mod error_boundary;
 
 pub(crate) mod accept_method;
 
-#[cfg(feature = "fs")]
-mod favicon;
-
 mod middleware;
 mod next;
 mod timeout;
-
-#[cfg(feature = "fs")]
-pub use favicon::favicon;
 
 pub use middleware::{Middleware, Result};
 pub use next::Next;
