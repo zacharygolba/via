b43e088c96a7a7dfe7939d14d8e24ed0066a340d
Zachary Golba <pino@hey.com>
2025-03-17 18:04:15 -0400

    feat(via): add a test to TeeBody

--- src/body/tee_body.rs
+++ src/body/tee_body.rs
@@ -138,3 +138,163 @@ impl Debug for TeeBody {
         f.debug_struct("TeeBody").finish()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use bytes::Bytes;
+    use http_body::{Body, Frame};
+    use std::collections::VecDeque;
+    use std::io;
+    use std::pin::Pin;
+    use std::sync::Arc;
+    use std::task::{Context, Poll, Waker};
+    use tokio::io::AsyncWrite;
+    use tokio::sync::Mutex;
+
+    use super::{BoxBody, TeeBody};
+    use crate::body::tee_body::IoState;
+    use crate::error::DynError;
+
+    struct MockBody {
+        ready: bool,
+        data: VecDeque<Result<Frame<Bytes>, DynError>>,
+    }
+
+    #[derive(Clone)]
+    struct MockWriter {
+        data: Arc<Mutex<Vec<u8>>>,
+    }
+
+    fn io_state_closed(state: &IoState) -> bool {
+        match state {
+            IoState::Writeable(_) | IoState::Shutdown(_) => false,
+            IoState::Closed => true,
+        }
+    }
+
+    fn expect_none(body: Pin<&mut TeeBody>, context: &mut Context) {
+        match body.poll_frame(context) {
+            Poll::Pending => panic!("expected ready, got pending"),
+            Poll::Ready(None) => {}
+            Poll::Ready(Some(_)) => panic!("expected none, got some"),
+        }
+    }
+
+    fn expect_data_frame(body: Pin<&mut TeeBody>, context: &mut Context) -> Bytes {
+        match body.poll_frame(context) {
+            Poll::Pending => panic!("expected ready, got pending"),
+            Poll::Ready(None) => panic!("expected some, got none"),
+            Poll::Ready(Some(result)) => result
+                .unwrap()
+                .into_data()
+                .expect("expected data, got trailers"),
+        }
+    }
+
+    impl MockBody {
+        fn new(
+            ready: bool,
+            data: impl IntoIterator<Item = Result<Frame<Bytes>, DynError>>,
+        ) -> Self {
+            Self {
+                ready,
+                data: data.into_iter().collect(),
+            }
+        }
+    }
+
+    impl Body for MockBody {
+        type Data = Bytes;
+        type Error = DynError;
+
+        fn poll_frame(
+            self: Pin<&mut Self>,
+            context: &mut Context,
+        ) -> Poll<Option<Result<Frame<Self::Data>, Self::Error>>> {
+            let this = self.get_mut();
+
+            if this.ready {
+                Poll::Ready(this.data.pop_front())
+            } else {
+                context.waker().wake_by_ref();
+                this.ready = true;
+                Poll::Pending
+            }
+        }
+    }
+
+    impl MockWriter {
+        fn new(data: Vec<u8>) -> Self {
+            Self {
+                data: Arc::new(Mutex::new(data)),
+            }
+        }
+    }
+
+    impl AsyncWrite for MockWriter {
+        fn poll_write(
+            self: Pin<&mut Self>,
+            context: &mut Context,
+            buf: &[u8],
+        ) -> Poll<Result<usize, io::Error>> {
+            match self.data.try_lock() {
+                Ok(mut guard) => {
+                    guard.extend_from_slice(buf);
+                    Poll::Ready(Ok(buf.len()))
+                }
+                Err(_) => {
+                    context.waker().wake_by_ref();
+                    Poll::Pending
+                }
+            }
+        }
+
+        fn poll_flush(self: Pin<&mut Self>, _: &mut Context) -> Poll<Result<(), io::Error>> {
+            Poll::Ready(Ok(()))
+        }
+
+        fn poll_shutdown(
+            self: Pin<&mut Self>,
+            context: &mut Context,
+        ) -> Poll<Result<(), io::Error>> {
+            eprintln!("poll_shutdown called on MockWriter");
+            self.poll_flush(context)
+        }
+    }
+
+    #[tokio::test]
+    async fn test_tee() {
+        let writer = MockWriter::new(vec![]);
+
+        let mut context = Context::from_waker(Waker::noop());
+        let mut body = TeeBody::new(
+            BoxBody::new(MockBody::new(
+                true,
+                vec![
+                    Ok(Frame::data(Bytes::copy_from_slice(b"hello "))),
+                    Ok(Frame::data(Bytes::copy_from_slice(b"world"))),
+                ],
+            )),
+            writer.clone(),
+        );
+
+        assert_eq!(
+            b"hello ".as_slice(),
+            expect_data_frame(Pin::new(&mut body), &mut context),
+        );
+
+        assert_eq!(
+            b"world".as_slice(),
+            expect_data_frame(Pin::new(&mut body), &mut context),
+        );
+
+        expect_none(Pin::new(&mut body), &mut context);
+
+        assert_eq!(
+            b"hello world".as_slice(),
+            writer.data.lock().await.as_slice()
+        );
+
+        assert!(io_state_closed(&body.state));
+    }
+}
