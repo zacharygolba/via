6fae3ae979219498263c5107fee0bc38384af170
Zachary Golba <pino@hey.com>
2025-04-09 14:49:41 -0400

    fix(via): timeout api

--- src/middleware/timeout.rs
+++ src/middleware/timeout.rs
@@ -4,28 +4,18 @@ use tokio::time;
 use super::{BoxFuture, Middleware, Next};
 use crate::{Error, Request};
 
-pub struct Timeout<T> {
+pub struct Timeout {
     duration: Duration,
-    middleware: T,
 }
 
 /// Create a new `Timeout` middleware with the specified duration.
-pub fn timeout<M, T>(duration: Duration, middleware: M) -> Timeout<M>
-where
-    M: Middleware<T>,
-{
-    Timeout {
-        duration,
-        middleware,
-    }
+pub fn timeout(duration: Duration) -> Timeout {
+    Timeout { duration }
 }
 
-impl<M, T> Middleware<T> for Timeout<M>
-where
-    M: Middleware<T>,
-{
+impl<T> Middleware<T> for Timeout {
     fn call(&self, request: Request<T>, next: Next<T>) -> BoxFuture {
-        let future = time::timeout(self.duration, self.middleware.call(request, next));
+        let future = time::timeout(self.duration, next.call(request));
 
         Box::pin(async {
             future.await.unwrap_or_else(|_| {
