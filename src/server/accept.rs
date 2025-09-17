65205137e98dd88ccd3933dc9140d5774ada4abb
Zachary Golba <pino@hey.com>
2025-09-17 19:04:51 -0400

    feat(via): 24 hr timeout on accept if connections.is_empty()

--- src/server/accept.rs
+++ src/server/accept.rs
@@ -18,6 +18,17 @@ use super::io::IoWithPermit;
 use crate::app::{App, AppService};
 use crate::error::ServerError;
 
+macro_rules! accept_with_timeout {
+    ($future:expr, $immediate:expr) => {
+        time::timeout(
+            // If $immediate timeout after 2 seconds of idle time to join a
+            // connection. Otherwise, timeout after a day.
+            Duration::from_secs(if $immediate { 2 } else { 60 * 60 * 24 }),
+            $future,
+        )
+    };
+}
+
 macro_rules! joined {
     ($result:expr) => {
         match $result {
@@ -88,12 +99,16 @@ where
             Err(_) => break ExitCode::FAILURE,
         };
 
+        if cfg!(debug_assertions) {
+            println!("available permits = {}", semaphore.available_permits());
+        }
+
         tokio::select! {
             // Poll the listener before anything else.
             biased;
 
             // Accept a connection from the TCP listener.
-            result = time::timeout(Duration::from_secs(1), listener.accept()) => {
+            result = accept_with_timeout!(listener.accept(), !connections.is_empty()) => {
                 match result {
                     // Connection accepted.
                     Ok(Ok((tcp_stream, _))) => {
