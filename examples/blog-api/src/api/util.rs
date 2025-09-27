8c70d4035f98a0d9aaac072323ba96a35d76e8e5
Zachary Golba <pino@hey.com>
2025-09-27 13:34:22 -0400

    fix(examples/blog-api): more specific error status codes

--- examples/blog-api/src/api/util.rs
+++ examples/blog-api/src/api/util.rs
@@ -1,4 +1,4 @@
-use diesel::result::Error as DieselError;
+use diesel::result::{DatabaseErrorKind, Error as DieselError};
 use http::StatusCode;
 use via::error::Sanitize;
 
@@ -9,18 +9,34 @@ pub fn with_error_sanitizer(error: Sanitize) -> Sanitize {
     eprintln!("error: {}", error);
 
     // Respond with json and sanitize potentially sensitive error messages.
-    error.as_json().map(|respond, source| {
+    error.as_json().map(|sanitize, source| {
         match source.downcast_ref() {
-            // The requested operation violates a database constraint.
-            Some(DieselError::DatabaseError(_, info)) if info.constraint_name().is_some() => {
-                respond.with_status_code(StatusCode::BAD_REQUEST)
+            Some(DieselError::DatabaseError(kind, _)) => {
+                if let Some(status_code) = status_for_database_error_kind(kind) {
+                    // The requested operation violates a database constraint.
+                    sanitize.with_status_code(status_code)
+                } else {
+                    // Opaque internal server error.
+                    sanitize.with_canonical_reason()
+                }
             }
 
             // The requested resource does not exist.
-            Some(DieselError::NotFound) => respond.with_status_code(StatusCode::NOT_FOUND),
+            Some(DieselError::NotFound) => sanitize.with_status_code(StatusCode::NOT_FOUND),
 
             // The error occured for some other reason.
-            _ => respond.with_canonical_reason(),
+            _ => sanitize.with_canonical_reason(),
         }
     })
+}
+
+fn status_for_database_error_kind(kind: &DatabaseErrorKind) -> Option<StatusCode> {
+    match kind {
+        DatabaseErrorKind::CheckViolation | DatabaseErrorKind::NotNullViolation => {
+            Some(StatusCode::BAD_REQUEST)
+        }
+        DatabaseErrorKind::ForeignKeyViolation => Some(StatusCode::UNPROCESSABLE_ENTITY),
+        DatabaseErrorKind::UniqueViolation => Some(StatusCode::CONFLICT),
+        _ => None,
+    }
 }
