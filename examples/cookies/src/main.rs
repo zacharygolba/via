978b78ff8eef6af959da8846a1b2f14160c19f2e
Zachary Golba <pino@hey.com>
2025-10-28 08:56:04 -0400

    fix(examples/cookies): use signed and encrypted cookies

--- examples/cookies/src/main.rs
+++ examples/cookies/src/main.rs
@@ -31,7 +31,7 @@ async fn counter(request: Request, next: Next) -> via::Result {
     //
     let value = request
         .cookies()
-        .signed(&state.secret)
+        .private(&state.secret)
         .get("counter")
         .map_or(Ok(0i32), |cookie| cookie.value().parse())?;
 
@@ -41,12 +41,11 @@ async fn counter(request: Request, next: Next) -> via::Result {
     // Print the number of times the user has visited the site to stdout.
     println!("User has visited {} times.", value);
 
-    // If the response status code is in 200..=299, increment the counter.
+    // If the response status code is in 200..=299, update the counter cookie.
     if response.status().is_success() {
         let incr = (value + 1).to_string();
-        let jar = response.cookies_mut();
 
-        jar.signed_mut(&state.secret).add(
+        response.cookies_mut().private_mut(&state.secret).add(
             Cookie::build(("counter", incr))
                 .same_site(SameSite::Strict)
                 .http_only(true)
@@ -82,8 +81,9 @@ async fn main() -> Result<ExitCode, Error> {
     // Create a new application.
     let mut app = App::new(CookiesExample {
         secret: env::var("VIA_SECRET_KEY")
-            .map(|secret| Key::from(secret.as_bytes()))
-            .expect("missing required env var: VIA_SECRET_KEY"),
+            .map(|secret| secret.as_bytes().try_into())
+            .expect("missing required env var: VIA_SECRET_KEY")
+            .expect("unexpected end of input while parsing VIA_SECRET_KEY"),
     });
 
     // The CookieParser middleware can be added at any depth of the route tree.
