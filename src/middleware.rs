2208ccd410fc8125b3171452a229aea7d746ad0d
Zachary Golba <pino@hey.com>
2025-10-29 16:10:56 -0400

    feat(via): BoxFuture is public API

--- src/middleware.rs
+++ src/middleware.rs
@@ -6,21 +6,27 @@ use crate::error::Error;
 use crate::request::Request;
 use crate::response::Response;
 
-/// The output of the `Future` returned from middleware.
+/// An alias for the pin
+/// [`Box<dyn Future>`]
+/// returned by
+/// [middleware](Middleware::call).
+///
+pub type BoxFuture<T = Result> = Pin<Box<dyn Future<Output = T> + Send>>;
+
+/// An alias for results that uses the [`Error`] struct defined in this crate.
 ///
 pub type Result<T = Response> = std::result::Result<T, Error>;
-pub type BoxFuture<T = Result> = Pin<Box<dyn Future<Output = T> + Send + 'static>>;
 
-pub trait Middleware<T>: Send + Sync {
-    fn call(&self, request: Request<T>, next: Next<T>) -> BoxFuture;
+pub trait Middleware<State>: Send + Sync {
+    fn call(&self, request: Request<State>, next: Next<State>) -> BoxFuture;
 }
 
-impl<M, T, F> Middleware<T> for M
+impl<State, F, R> Middleware<State> for F
 where
-    M: Fn(Request<T>, Next<T>) -> F + Send + Sync,
-    F: Future<Output = Result> + Send + 'static,
+    F: Fn(Request<State>, Next<State>) -> R + Send + Sync,
+    R: Future<Output = Result> + Send + 'static,
 {
-    fn call(&self, request: Request<T>, next: Next<T>) -> BoxFuture {
+    fn call(&self, request: Request<State>, next: Next<State>) -> BoxFuture {
         Box::pin(self(request, next))
     }
 }
