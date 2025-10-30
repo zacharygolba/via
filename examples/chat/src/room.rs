7ae3e2e8b592c9ec6300ff529860ab134b31b01e
Zachary Golba <pino@hey.com>
2025-10-30 16:42:29 -0400

    refactor(via): correctness improvements to the websocket API

--- examples/chat/src/room.rs
+++ examples/chat/src/room.rs
@@ -1,14 +1,24 @@
-use serde_json::json;
-use via::ws::{self, Message};
+use bytestring::ByteString;
+use serde::Serialize;
+use via::websocket::{Channel, Context, Message};
 use via::{Next, Payload, Request, Response, raise};
 
 use crate::chat::Chat;
 
+#[derive(Serialize)]
+struct ChatThread<'a> {
+    messages: &'a [ByteString],
+}
+
+#[derive(Serialize)]
+struct ChatMessage {
+    message: ByteString,
+}
+
 pub async fn index(request: Request<Chat>, _: Next<Chat>) -> via::Result {
     let slug = request.param("room").into_result()?;
     let all = request.state().all(&slug, |messages| {
-        let body = json!({ "data": { "messages": messages } });
-        Response::build().json(&body)
+        Response::build().json(&ChatThread { messages })
     });
 
     all.await.unwrap_or_else(|| raise!(404))
@@ -21,45 +31,53 @@ pub async fn show(request: Request<Chat>, _: Next<Chat>) -> via::Result {
         raise!(404)
     };
 
-    let data = json!({ "message": &message });
-    Response::build().json(&data)
+    Response::build().json(&ChatMessage { message })
 }
 
-pub async fn join(mut channel: ws::Channel, request: ws::Context<Chat>) -> via::Result<()> {
-    let slug = request.param("room").into_result()?;
-    let chat = request.state();
+pub async fn join(mut channel: Channel, context: Context<Chat>) -> via::Result<()> {
+    let slug = context.param("room").into_result()?.into_owned();
+    let state = context.into_state();
 
-    let (id, (tx, mut rx)) = chat.join(&slug).await;
+    let (id, pubsub) = state.join(&slug).await;
+    let (tx, mut rx) = pubsub;
 
     loop {
         tokio::select! {
             // Update received from the room's broadcast channel.
             Ok((from, index)) = rx.recv() => {
                 if id == from {
-                    continue; // Skip updates if they came from us.
+                    continue;
                 }
 
-                if let Some(message) = chat.get(&slug, index).await {
-                    channel.send(message).await?;
+                if let Some(content) = state.get(&slug, index).await {
+                    channel.send(content).await?;
                 }
             }
 
             // Message received from the websocket.
-            Some(message) = channel.next() => {
-                // Break the loop when we receive a close message.
-                if let Message::Close(close) = &message {
-                    if let Some((code, reason)) = close {
-                        let code = u16::from(*code);
-                        let reason = reason.as_deref().unwrap_or("reason not provided");
+            Some(next) = channel.next() => match next {
+                // Append the message content to the chat thread.
+                content @ (Message::Binary(_) | Message::Text(_)) => {
+                    let message = content.into_utf8()?;
 
-                        eprintln!("close(room: {}, code: {}): {}", &slug, code, reason);
+                    if let Some(index) = state.push(&slug, message).await {
+                        let _ = tx.send((id, index));
                     }
+                }
+
+                // Break the loop when we receive a close message.
+                Message::Close(close) => {
+                    close.as_ref().inspect(|(code, reason)| {
+                        let reason = reason.as_deref().unwrap_or("reason not provided");
+                        eprintln!("close(room: {}, code: {:?}): {}", slug, code, reason);
+                    });
 
                     break Ok(());
                 }
 
-                if let Some(index) = chat.push(&slug, message.into_utf8()?).await {
-                    let _ = tx.send((id, index));
+                // Print a warning to stderr for ignored messages.
+                ignore => {
+                    eprintln!("warn(room: {}): ignored message in room::join {:?}", slug, ignore);
                 }
             },
         }
