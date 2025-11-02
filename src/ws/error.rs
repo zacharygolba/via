dc747fcb7124d6b85009a8251ddd6fa23d8da1db
Zachary Golba <pino@hey.com>
2025-11-02 10:57:53 -0500

    fix(via): cleanup ws code

---
+++ src/ws/error.rs
@@ -0,0 +1,74 @@
+use std::fmt::{self, Display, Formatter};
+use std::ops::ControlFlow::{self, Break, Continue};
+use tokio_websockets::Error as WebSocketError;
+
+use crate::error::Error;
+
+pub type Result<T = ()> = std::result::Result<T, ControlFlow<Error, Error>>;
+
+pub trait Retry {
+    type Output;
+    fn or_break(self) -> Result<Self::Output>;
+    fn or_continue(self) -> Result<Self::Output>;
+}
+
+#[derive(Debug)]
+pub enum ErrorKind {
+    Runtime(Error),
+    Socket(WebSocketError),
+}
+
+pub fn try_rescue_ws(error: WebSocketError) -> ControlFlow<Option<ErrorKind>, Option<ErrorKind>> {
+    let into_control_flow = match &error {
+        WebSocketError::PayloadTooLong { .. } | WebSocketError::Protocol(_) => Continue,
+        WebSocketError::Io(source) => match source.kind() {
+            std::io::ErrorKind::Interrupted
+            | std::io::ErrorKind::TimedOut
+            | std::io::ErrorKind::WouldBlock
+            | std::io::ErrorKind::WriteZero => Continue,
+            _ => Break,
+        },
+        _ => Break,
+    };
+
+    into_control_flow(Some(error.into()))
+}
+
+impl ErrorKind {
+    pub const CLOSED: Self = Self::Socket(WebSocketError::AlreadyClosed);
+}
+
+impl std::error::Error for ErrorKind {}
+
+impl Display for ErrorKind {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            Self::Runtime(error) => Display::fmt(error, f),
+            Self::Socket(error) => Display::fmt(error, f),
+        }
+    }
+}
+
+impl From<Error> for ErrorKind {
+    fn from(error: Error) -> Self {
+        Self::Runtime(error)
+    }
+}
+
+impl From<WebSocketError> for ErrorKind {
+    fn from(error: WebSocketError) -> Self {
+        Self::Socket(error)
+    }
+}
+
+impl<T> Retry for crate::Result<T> {
+    type Output = T;
+
+    fn or_break(self) -> Result<Self::Output> {
+        self.map_err(ControlFlow::Break)
+    }
+
+    fn or_continue(self) -> Result<Self::Output> {
+        self.map_err(ControlFlow::Continue)
+    }
+}
