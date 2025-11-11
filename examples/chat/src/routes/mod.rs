37616830c57484fcb3f1cc1a5a567304b5644e9f
Zachary Golba <pino@hey.com>
2025-11-11 09:26:41 -0500

    fix(examples/chat): move auth routes to /api/auth

--- examples/chat/src/routes/mod.rs
+++ examples/chat/src/routes/mod.rs
@@ -1,3 +1,4 @@
+pub mod auth;
 pub mod messages;
 pub mod reactions;
 pub mod threads;
--- examples/chat/src/util/mod.rs
+++ examples/chat/src/util/mod.rs
@@ -1,9 +1,10 @@
-mod auth;
+pub mod auth;
+
 mod error;
 mod id;
 mod paginate;
 
-pub use auth::{Auth, Session};
+pub use auth::{Authenticate, Session};
 pub use error::{FoundOrForbidden, error_sanitizer};
 pub use id::Id;
 pub use paginate::{Cursor, LimitAndOffset};
